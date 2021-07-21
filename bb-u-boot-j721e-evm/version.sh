a615b675c18c64779c4dec388751aae86950d2ba
Robert Nelson <robertcnelson@gmail.com>
2021-07-21 14:13:06 -0500

    bb-u-boot-j721e-evm (2021.07.20210720.11)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- bb-u-boot-j721e-evm/version.sh
+++ bb-u-boot-j721e-evm/version.sh
@@ -4,7 +4,7 @@
 
 package_name="bb-u-boot-j721e-evm"
 debian_pkg_name="${package_name}"
-package_version="2021.07.20210720.10"
+package_version="2021.07.20210720.11"
 package_source=""
 src_dir=""
 
@@ -16,5 +16,9 @@ dl_path="pool/main/${reprepro_dir}/"
 debian_version="${package_version}-0"
 debian_untar=""
 debian_patch=""
+
+debian_dl_1="https://github.com/beagleboard/j721e-evm/raw/main/bootloader/deploy/2021-07-21/sysfw.itb"
+debian_dl_2="https://github.com/beagleboard/j721e-evm/raw/main/bootloader/deploy/2021-07-21/bl31.bin"
+debian_dl_3="https://github.com/beagleboard/j721e-evm/raw/main/bootloader/deploy/2021-07-21/tee-pager_v2.bin"
 
 bullseye_version="~bullseye+20210721"
