1fea8d5493c8468ed7ce9d6b157ef56a3c0066f1
Robert Nelson <robertcnelson@gmail.com>
2021-08-07 10:55:56 -0500

    bb-u-boot-j721e-evm (2021.10.20210726.0)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- bb-u-boot-j721e-evm/version.sh
+++ bb-u-boot-j721e-evm/version.sh
@@ -4,7 +4,7 @@
 
 package_name="bb-u-boot-j721e-evm"
 debian_pkg_name="${package_name}"
-package_version="2021.07.20210722.0"
+package_version="2021.10.20210726.0"
 package_source=""
 src_dir=""
 
@@ -17,14 +17,17 @@ debian_version="${package_version}-0"
 debian_untar=""
 debian_patch=""
 
-debian_dl_1="https://github.com/beagleboard/j721e-evm/raw/main/bootloader/deploy/2021-07-21/bl31.bin"
-debian_dl_2="https://github.com/beagleboard/j721e-evm/raw/main/bootloader/deploy/2021-07-21/tee-pager_v2.bin"
+#projectdir="2021-07-21"
+#debian_dl_1="https://github.com/beagleboard/j721e-evm/raw/main/bootloader/deploy/${projectdir}/bl31.bin"
+#debian_dl_2="https://github.com/beagleboard/j721e-evm/raw/main/bootloader/deploy/${projectdir}/tee-pager_v2.bin"
+#debian_dl_3="https://github.com/beagleboard/j721e-evm/raw/main/bootloader/deploy/${projectdir}/sysfw.itb"
 
-#debian_dl_3="https://github.com/beagleboard/j721e-evm/raw/main/bootloader/deploy/2021-07-21/sysfw.itb"
-debian_dl_3="https://github.com/beagleboard/j721e-evm/raw/main/bootloader/deploy/07.03.00.05/sysfw.itb"
+projectdir="CoreSDK-08.00.00.004"
+debian_dl_1="https://github.com/beagleboard/j721e-evm/raw/main/bootloader/deploy/${projectdir}/bl31.bin"
+debian_dl_2="https://github.com/beagleboard/j721e-evm/raw/main/bootloader/deploy/${projectdir}/tee-pager_v2.bin"
+debian_dl_3="https://github.com/beagleboard/j721e-evm/raw/main/bootloader/deploy/${projectdir}/sysfw.itb"
+debian_dl_4="https://github.com/beagleboard/j721e-evm/raw/main/bootloader/deploy/${projectdir}/tiboot3.bin"
+debian_dl_5="https://github.com/beagleboard/j721e-evm/raw/main/bootloader/deploy/${projectdir}/tispl.bin"
+debian_dl_6="https://github.com/beagleboard/j721e-evm/raw/main/bootloader/deploy/${projectdir}/u-boot.img"
 
-debian_dl_4="https://github.com/beagleboard/j721e-evm/raw/main/bootloader/deploy/07.03.00.05/tiboot3.bin"
-debian_dl_5="https://github.com/beagleboard/j721e-evm/raw/main/bootloader/deploy/07.03.00.05/tispl.bin"
-debian_dl_6="https://github.com/beagleboard/j721e-evm/raw/main/bootloader/deploy/07.03.00.05/u-boot.img"
-
-bullseye_version="~bullseye+20210722"
+bullseye_version="~bullseye+20210807"
