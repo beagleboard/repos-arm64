7dd72c1c3d5b769062ccc8174b4189b3d325013a
Robert Nelson <robertcnelson@gmail.com>
2021-08-07 20:53:28 -0500

    bb-j721e-evm-firmware (1.20210807.0)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

---
+++ bb-j721e-evm-firmware/get_latest.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+dl_src () {
+	if [ -d ./src/ ] ; then
+		rm -rf ./src/
+	fi
+
+	git clone git@github.com:beagleboard/j721e-evm.git ./src/
+}
+
+copy_all () {
+	cp -v ./src${wdir}${wfile} ./suite/bullseye/debian/
+}
+
+dl_src
+
+wfile="j7-c66_0-fw.tisdk" ; wdir="/fw/CoreSDK-08.00.00.004/" ; copy_all
+wfile="j7-c66_1-fw.tisdk" ; wdir="/fw/CoreSDK-08.00.00.004/" ; copy_all
+wfile="j7-c71_0-fw.tisdk" ; wdir="/fw/CoreSDK-08.00.00.004/" ; copy_all
+wfile="j7-main-r5f0_0-fw.tisdk" ; wdir="/fw/CoreSDK-08.00.00.004/" ; copy_all
+wfile="j7-main-r5f0_1-fw.tisdk" ; wdir="/fw/CoreSDK-08.00.00.004/" ; copy_all
+wfile="j7-main-r5f1_0-fw.tisdk" ; wdir="/fw/CoreSDK-08.00.00.004/" ; copy_all
+wfile="j7-main-r5f1_1-fw.tisdk" ; wdir="/fw/CoreSDK-08.00.00.004/" ; copy_all
+wfile="j7-mcu-r5f0_0-fw.tisdk" ; wdir="/fw/CoreSDK-08.00.00.004/" ; copy_all
+wfile="j7-mcu-r5f0_1-fw.tisdk" ; wdir="/fw/CoreSDK-08.00.00.004/" ; copy_all
+
---
+++ tools/update_changelog.sh
@@ -0,0 +1,47 @@
+#!/bin/bash -e
+
+. version.sh
+
+DIR="$PWD"
+
+run () {
+	wfile="${DIR}/suite/${suite}/debian/new"
+
+	if [ "x${debian_version}" = "x" ] ; then
+		if [ "x${epoch}" = "x" ] ; then
+			echo "${debian_pkg_name} (${package_version}${local_patch}~${suite}+${simple_date}) ${suite}; urgency=low" > ${wfile}
+		else
+			echo "${debian_pkg_name} (${epoch}:${package_version}${local_patch}~${suite}+${simple_date}) ${suite}; urgency=low" > ${wfile}
+		fi
+	else
+		if [ "x${epoch}" = "x" ] ; then
+			echo "${debian_pkg_name} (${debian_version}${local_patch}~${suite}+${simple_date}) ${suite}; urgency=low" > ${wfile}
+		else
+			echo "${debian_pkg_name} (${epoch}:${debian_version}${local_patch}~${suite}+${simple_date}) ${suite}; urgency=low" > ${wfile}
+		fi
+	fi
+	echo "" >> ${wfile}
+	echo "  * Rebuild for repos.rcn-ee.com" >> ${wfile}
+	echo "" >> ${wfile}
+	echo " -- Robert Nelson <robertcnelson@gmail.com>  ${new_date}" >> ${wfile}
+	echo "" >> ${wfile}
+
+	cat ${DIR}/suite/${suite}/debian/changelog >> ${wfile}
+	rm ${DIR}/suite/${suite}/debian/changelog
+	mv ${wfile} ${DIR}/suite/${suite}/debian/changelog
+}
+
+new_date=`LANG=C date -R`
+simple_date=`LANG=C date +%Y%m%d`
+
+dist="debian"
+suite="bullseye"
+if [ -d ${DIR}/suite/${suite}/ ] ; then
+	rcn_ee_version="${bullseye_version}"
+	cat ${DIR}/version.sh | grep -v bullseye_version > ${DIR}/new-version.sh
+	echo "${suite}_version=\"~${suite}+${simple_date}\"" >> ${DIR}/new-version.sh
+	mv ${DIR}/new-version.sh ${DIR}/version.sh
+	run
+fi
+
+#
