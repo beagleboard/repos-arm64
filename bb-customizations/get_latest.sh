a59fde361be6f82b66c07ba80c6783b932b4a75f
Robert Nelson <robertcnelson@gmail.com>
2021-08-08 13:32:27 -0500

    bb-customizations (1.20210808.0)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

---
+++ bb-customizations/get_latest.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+dl_src () {
+	if [ -d ./src/ ] ; then
+		rm -rf ./src/
+	fi
+
+	git clone git@github.com:beagleboard/j721e-evm.git ./src/
+}
+
+copy_all () {
+	cp -v ./src${wdir}${wfile} ./suite/bullseye/debian/
+}
+
+dl_src
+
+wfile="86-remoteproc-noroot.rules" ; wdir="/rootfs/etc/udev/rules.d/" ; copy_all
