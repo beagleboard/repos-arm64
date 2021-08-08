e5a8ed4864a6150f6e10a51424f1ebe2cf5434e9
Robert Nelson <robertcnelson@gmail.com>
2021-08-08 12:19:12 -0500

    pull from git directly
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- bb-u-boot-j721e-evm/version.sh
+++ bb-u-boot-j721e-evm/version.sh
@@ -17,17 +17,4 @@ debian_version="${package_version}-0"
 debian_untar=""
 debian_patch=""
 
-#projectdir="2021-07-21"
-#debian_dl_1="https://github.com/beagleboard/j721e-evm/raw/main/bootloader/deploy/${projectdir}/bl31.bin"
-#debian_dl_2="https://github.com/beagleboard/j721e-evm/raw/main/bootloader/deploy/${projectdir}/tee-pager_v2.bin"
-#debian_dl_3="https://github.com/beagleboard/j721e-evm/raw/main/bootloader/deploy/${projectdir}/sysfw.itb"
-
-projectdir="CoreSDK-08.00.00.004"
-debian_dl_1="https://github.com/beagleboard/j721e-evm/raw/main/bootloader/deploy/${projectdir}/bl31.bin"
-debian_dl_2="https://github.com/beagleboard/j721e-evm/raw/main/bootloader/deploy/${projectdir}/tee-pager_v2.bin"
-debian_dl_3="https://github.com/beagleboard/j721e-evm/raw/main/bootloader/deploy/${projectdir}/sysfw.itb"
-debian_dl_4="https://github.com/beagleboard/j721e-evm/raw/main/bootloader/deploy/${projectdir}/tiboot3.bin"
-debian_dl_5="https://github.com/beagleboard/j721e-evm/raw/main/bootloader/deploy/${projectdir}/tispl.bin"
-debian_dl_6="https://github.com/beagleboard/j721e-evm/raw/main/bootloader/deploy/${projectdir}/u-boot.img"
-
 bullseye_version="~bullseye+20210807"
