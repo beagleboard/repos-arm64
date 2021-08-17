8179013f9e9abd94257772eb81c573d254a9a778
Robert Nelson <robertcnelson@gmail.com>
2021-08-17 12:21:33 -0500

    cloud-initramfs-tools (0.47ubuntu1rcnee0)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

---
+++ debian-10-cloud-initramfs-tools/version.sh
@@ -0,0 +1,24 @@
+#!/bin/bash -e
+
+#https://git.launchpad.net/~cloud-initramfs-tools/cloud-initramfs-tools
+#https://packages.ubuntu.com/source/focal/cloud-initramfs-tools
+#mirror="http://http.debian.net/debian"
+mirror="http://archive.ubuntu.com/ubuntu"
+
+package_name="cloud-initramfs-tools"
+debian_pkg_name="${package_name}"
+package_version="0.47ubuntu1"
+package_source="${debian_pkg_name}_${package_version}.tar.xz"
+src_dir="${package_name}-${package_version}"
+
+git_repo=""
+git_sha=""
+reprepro_dir="c/${package_name}"
+dl_path="pool/main/${reprepro_dir}/"
+
+debian_version="${package_version}"
+debian_untar=""
+debian_patch=""
+local_patch="rcnee0"
+
+bullseye_version="~bullseye+20210817"
