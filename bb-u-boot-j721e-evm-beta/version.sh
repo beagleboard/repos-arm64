d243018dcf6e1200397b56e41f76c2acfda75c28
Robert Nelson <robertcnelson@gmail.com>
2021-08-31 09:47:07 -0500

    bb-u-boot-j721e-evm-beta (2021.01.20210831.2)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- bb-u-boot-j721e-evm-beta/version.sh
+++ bb-u-boot-j721e-evm-beta/version.sh
@@ -4,7 +4,7 @@
 
 package_name="bb-u-boot-j721e-evm-beta"
 debian_pkg_name="${package_name}"
-package_version="2021.01.20210831.1"
+package_version="2021.01.20210831.2"
 package_source=""
 src_dir=""
 
