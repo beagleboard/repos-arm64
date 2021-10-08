0b901fda551ce45d944595eda7630cca7cb957ed
Robert Nelson <robertcnelson@gmail.com>
2021-10-07 21:24:15 -0500

    bb-customizations (1.20211007.0)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- bb-customizations/version.sh
+++ bb-customizations/version.sh
@@ -4,7 +4,7 @@
 
 package_name="bb-customizations"
 debian_pkg_name="${package_name}"
-package_version="1.20210808.0"
+package_version="1.20211007.0"
 package_source=""
 src_dir=""
 
@@ -17,4 +17,4 @@ debian_version="${package_version}-0"
 debian_untar=""
 debian_patch=""
 
-bullseye_version="~bullseye+20210808"
+bullseye_version="~bullseye+20211007"
