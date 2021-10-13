09c3e6da82a1fbaf5c43fad697047ccd560e7839
Robert Nelson <robertcnelson@gmail.com>
2021-10-13 10:55:03 -0500

    bb-usb-gadgets (1.20211012.0)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- bb-usb-gadgets/version.sh
+++ bb-usb-gadgets/version.sh
@@ -2,7 +2,7 @@
 
 package_name="bb-usb-gadgets"
 debian_pkg_name="${package_name}"
-package_version="1.20211007.2"
+package_version="1.20211012.0"
 package_source=""
 src_dir=""
 
@@ -15,4 +15,4 @@ debian_version="${package_version}-0"
 debian_untar=""
 debian_patch=""
 
-bullseye_version="~bullseye+20211007"
+bullseye_version="~bullseye+20211013"
