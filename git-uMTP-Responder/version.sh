d3a594168fb5b2bdf36f82bc1c71010b701e2726
Robert Nelson <robertcnelson@gmail.com>
2021-10-13 10:45:58 -0500

    umtp-responder (1.4.3-git20210912.0-0rcnee2)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- git-uMTP-Responder/version.sh
+++ git-uMTP-Responder/version.sh
@@ -7,7 +7,7 @@
 package_name="umtp-responder"
 debian_pkg_name="${package_name}"
 package_version="1.4.3-git20210912.0"
-package_source="${package_name}-${package_version}.orig.tar.xz"
+package_source="${package_name}_${package_version}.orig.tar.xz"
 src_dir="${package_name}-${package_version}"
 
 git_repo="https://github.com/viveris/uMTP-Responder"
@@ -15,7 +15,7 @@ git_sha="e140e559edc914976e10bcab13d37a56c4c505b2"
 reprepro_dir="u/${package_name}"
 dl_path=""
 
-debian_version="${package_version}-0rcnee1"
+debian_version="${package_version}-0rcnee2"
 debian_patch=""
 debian_diff=""
 
