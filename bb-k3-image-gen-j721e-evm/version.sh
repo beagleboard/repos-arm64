37e5ef734a4b261d55923a3baaa5eb95bad5c816
Robert Nelson <robertcnelson@gmail.com>
2021-10-15 16:06:23 -0500

    bb-k3-image-gen-j721e-evm (1.20210626.0)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

---
+++ bb-k3-image-gen-j721e-evm/version.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -e
+
+#https://github.com/beagleboard/k3-image-gen
+
+package_name="bb-k3-image-gen-j721e-evm"
+debian_pkg_name="${package_name}"
+package_version="1.20210626.0"
+package_source=""
+src_dir=""
+
+git_repo=""
+git_sha=""
+reprepro_dir="b/${package_name}"
+dl_path="pool/main/${reprepro_dir}/"
+
+debian_version="${package_version}-0"
+debian_untar=""
+debian_patch=""
+
+bullseye_version="~bullseye+20211015"
