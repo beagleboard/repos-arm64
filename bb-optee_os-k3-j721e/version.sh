c785b322ba456b65903dc33ac3f0993193afb243
Robert Nelson <robertcnelson@gmail.com>
2021-10-15 16:54:18 -0500

    bb-optee_os-k3-j721e (3.12.0.20210120.0)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

---
+++ bb-optee_os-k3-j721e/version.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -e
+
+#https://github.com/beagleboard/optee_os
+
+package_name="bb-optee_os-k3-j721e"
+debian_pkg_name="${package_name}"
+package_version="3.12.0.20210120.0"
+package_source=""
+src_dir=""
+
+git_repo=""
+git_sha=""
+reprepro_dir="b/${package_name}"
+dl_path="pool/main/${reprepro_dir}/"
+
+debian_version="${package_version}-0"
+debian_untar=""
+debian_patch=""
+
+bullseye_version="~bullseye+20211015"
