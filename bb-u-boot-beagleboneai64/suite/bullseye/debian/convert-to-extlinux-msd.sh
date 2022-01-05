a57251393ba2325b98bbcd287e2a3f5cae2d6b7c
Robert Nelson <robertcnelson@gmail.com>
2022-01-05 15:11:19 -0600

    bb-u-boot-beagleboneai64 (2021.10.20220105.3)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

---
+++ bb-u-boot-beagleboneai64/suite/bullseye/debian/convert-to-extlinux-msd.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+if ! id | grep -q root; then
+	echo "must be run as root"
+	exit
+fi
+
+wdir="/opt/u-boot/bb-u-boot-beagleboneai64"
+
+if [ -d /boot/firmware/ ] ; then
+	mkdir -p /boot/firmware/extlinux/
+	cp -v ${wdir}/msd-extlinux.conf /boot/firmware/extlinux/extlinux.conf
+	sudo cp -v /boot/Image-`uname -r` /boot/firmware/Image
+	sudo cp -v /boot/*.dtb /boot/firmware/
+fi
