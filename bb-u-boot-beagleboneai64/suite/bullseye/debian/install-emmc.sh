b9d40fc084f1e2ebb92486235656e896874c30b8
Robert Nelson <robertcnelson@gmail.com>
2022-01-05 10:51:50 -0600

    bb-u-boot-beagleboneai64 (2021.10.20211103.2)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- bb-u-boot-beagleboneai64/suite/bullseye/debian/install-emmc.sh
+++ bb-u-boot-beagleboneai64/suite/bullseye/debian/install-emmc.sh
@@ -8,12 +8,12 @@ fi
 wdir="/opt/u-boot/bb-u-boot-beagleboneai64"
 
 if [ -d /boot/firmware/ ] ; then
-	mmc extcsd read /dev/mmcblk0
+	#mmc extcsd read /dev/mmcblk0
 	mmc bootpart enable 1 2 /dev/mmcblk0
 	mmc bootbus set single_backward x1 x8 /dev/mmcblk0
 	echo '0' >> /sys/class/block/mmcblk0boot0/force_ro
 	dd if=tiboot3.bin of=/dev/mmcblk0boot0 count=3 bs=128k
 	dd if=tispl.bin of=/dev/mmcblk0boot0 seek=3 count=6 bs=128k
 	dd if=u-boot.img of=/dev/mmcblk0boot0 seek=9 count=16 bs=128k
-	dd if=sysfw.itb of=/dev/mmcblk0boot0 seek=25 count=3 bs=128k
+	dd if=/opt/u-boot/bb-k3-image-gen-j721e-evm/sysfw.itb of=/dev/mmcblk0boot0 seek=25 count=3 bs=128k
 fi
