e2e9cef39fb2b4759faf57012e4119b95388c5e7
Robert Nelson <robertcnelson@gmail.com>
2022-01-11 17:18:01 -0600

    bb-u-boot-beagleboneai64 (2021.10.20220111.5)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- bb-u-boot-beagleboneai64/version.sh
+++ bb-u-boot-beagleboneai64/version.sh
@@ -4,7 +4,7 @@
 
 package_name="bb-u-boot-beagleboneai64"
 debian_pkg_name="${package_name}"
-package_version="2021.10.20220111.4"
+package_version="2021.10.20220111.5"
 package_source=""
 src_dir=""
 
