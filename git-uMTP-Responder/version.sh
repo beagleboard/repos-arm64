6e93b9232ee4d445b7223119a675b5ba990b23bc
Robert Nelson <robertcnelson@gmail.com>
2022-01-12 14:12:08 -0600

    umtp-responder (1.6.2-git20211226.1)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- git-uMTP-Responder/version.sh
+++ git-uMTP-Responder/version.sh
@@ -6,7 +6,7 @@
 
 package_name="umtp-responder"
 debian_pkg_name="${package_name}"
-package_version="1.6.2-git20211226.0"
+package_version="1.6.2-git20211226.1"
 package_source="${package_name}_${package_version}.orig.tar.xz"
 src_dir="${package_name}_${package_version}"
 
@@ -15,7 +15,7 @@ git_sha="73287dfd9d6ec187538d76c062a09d6757efea9d"
 reprepro_dir="u/${package_name}"
 dl_path=""
 
-debian_version="${package_version}-0rcnee0"
+debian_version="${package_version}-0"
 debian_patch=""
 debian_diff=""
 
