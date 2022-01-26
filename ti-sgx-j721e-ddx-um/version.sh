29a6386c6b080aa6b8b5f32111482fc42c9584d9
Robert Nelson <robertcnelson@gmail.com>
2022-01-26 14:27:44 -0600

    ti-sgx-j721e-ddx-um (1.13.5776728-k5.10-git20220126.0)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- ti-sgx-j721e-ddx-um/version.sh
+++ ti-sgx-j721e-ddx-um/version.sh
@@ -4,12 +4,12 @@
 
 package_name="ti-sgx-j721e-ddx-um"
 debian_pkg_name="${package_name}"
-package_version="1.13.5776728-k5.10-git20210323.5"
+package_version="1.13.5776728-k5.10-git20220126.0"
 package_source="${package_name}_${package_version}.orig.tar.xz"
 src_dir="${package_name}_${package_version}"
 
 git_repo="git://git.ti.com/graphics/ti-img-rogue-umlibs.git"
-git_sha="5ef38353c38d921a65ea6df75c7b6ce710b66034"
+git_sha="258add91efa402d3647302165373c6a8d1b7201d"
 reprepro_dir="t/${package_name}"
 dl_path=""
 
@@ -17,4 +17,6 @@ debian_version="${package_version}-0"
 debian_patch=""
 debian_diff=""
 
-bullseye_version="~bullseye+20211104"
+clear_changelog="enable"
+
+bullseye_version="~bullseye+20220126"
