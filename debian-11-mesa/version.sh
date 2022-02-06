36caeb841f1357e9e2424a0f6cfacbdabab7db8d
Robert Nelson <robertcnelson@gmail.com>
2022-02-06 14:02:31 -0600

    mesa (21.2.6-1rcnee0)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- debian-11-mesa/version.sh
+++ debian-11-mesa/version.sh
@@ -2,11 +2,11 @@
 
 #mirror="http://http.debian.net/debian"
 #https://snapshot.debian.org/package/mesa/
-mirror="https://snapshot.debian.org/archive/debian/20210803T153742Z/"
+mirror="https://snapshot.debian.org/archive/debian/20211125T203520Z/"
 
 package_name="mesa"
 debian_pkg_name="${package_name}"
-package_version="21.1.6"
+package_version="21.2.6"
 package_source="${debian_pkg_name}_${package_version}.orig.tar.gz"
 src_dir="${package_name}-${package_version}"
 
