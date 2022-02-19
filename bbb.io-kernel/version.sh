abbfe1d5cd53aceae494b365eeee3fbd47c0eabd
Robert Nelson <robertcnelson@gmail.com>
2022-02-19 12:48:01 -0600

    bbb.io-kernel (1.20220219.0)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- bbb.io-kernel/version.sh
+++ bbb.io-kernel/version.sh
@@ -2,7 +2,7 @@
 
 package_name="bbb.io-kernel"
 debian_pkg_name="${package_name}"
-package_version="1.20220216.0"
+package_version="1.20220219.0"
 package_source=""
 src_dir=""
 
@@ -17,4 +17,4 @@ debian_patch=""
 
 clear_changelog="enable"
 
-bullseye_version="~bullseye+20220216"
+bullseye_version="~bullseye+20220219"
