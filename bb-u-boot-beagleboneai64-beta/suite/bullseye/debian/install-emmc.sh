7cc7cc718e96c39301bd0030b484e98d1a94c4a5
Robert Nelson <robertcnelson@gmail.com>
2022-03-02 11:13:22 -0600

    bb-u-boot-beagleboneai64-beta (2021.10.20220302.0)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- bb-u-boot-beagleboneai64-beta/suite/bullseye/debian/install-emmc.sh
+++ bb-u-boot-beagleboneai64-beta/suite/bullseye/debian/install-emmc.sh
@@ -14,7 +14,13 @@ if [ -d /boot/firmware/ ] ; then
 	mmc hwreset enable /dev/mmcblk0
 	echo '0' >> /sys/class/block/mmcblk0boot0/force_ro
 	dd if=${wdir}/tiboot3.bin of=/dev/mmcblk0boot0 count=3 bs=128k
-	dd if=${wdir}/tispl.bin of=/dev/mmcblk0boot0 seek=3 count=6 bs=128k
-	dd if=${wdir}/u-boot.img of=/dev/mmcblk0boot0 seek=9 count=16 bs=128k
-	dd if=/opt/u-boot/bb-k3-image-gen-j721e-evm/sysfw.itb of=/dev/mmcblk0boot0 seek=25 count=3 bs=128k
+	#legacy, we just doing FS mode now...
+	dd if=/dev/zero of=/dev/mmcblk0boot0 seek=3 count=6 bs=128k
+	dd if=/dev/zero of=/dev/mmcblk0boot0 seek=9 count=16 bs=128k
+	dd if=/dev/zero of=/dev/mmcblk0boot0 seek=25 count=3 bs=128k
+
+	cp -v /opt/u-boot/bb-k3-image-gen-j721e-evm/sysfw.itb /boot/firmware/
+	cp -v ${wdir}/tiboot3.bin /boot/firmware/
+	cp -v ${wdir}/tispl.bin /boot/firmware/
+	cp -v ${wdir}/u-boot.img /boot/firmware/
 fi
