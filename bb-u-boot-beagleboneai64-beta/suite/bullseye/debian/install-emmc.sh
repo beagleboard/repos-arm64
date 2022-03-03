70d827a977fe4e5cd0aebefc1f96f73f59a975db
Robert Nelson <robertcnelson@gmail.com>
2022-03-03 14:27:36 -0600

    bb-u-boot-beagleboneai64-beta (2021.10.20220302.1)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- bb-u-boot-beagleboneai64-beta/suite/bullseye/debian/install-emmc.sh
+++ bb-u-boot-beagleboneai64-beta/suite/bullseye/debian/install-emmc.sh
@@ -12,8 +12,10 @@ if [ -d /boot/firmware/ ] ; then
 	mmc bootpart enable 1 2 /dev/mmcblk0
 	mmc bootbus set single_backward x1 x8 /dev/mmcblk0
 	mmc hwreset enable /dev/mmcblk0
+
 	echo '0' >> /sys/class/block/mmcblk0boot0/force_ro
 	dd if=${wdir}/tiboot3.bin of=/dev/mmcblk0boot0 count=3 bs=128k
+
 	#legacy, we just doing FS mode now...
 	dd if=/dev/zero of=/dev/mmcblk0boot0 seek=3 count=6 bs=128k
 	dd if=/dev/zero of=/dev/mmcblk0boot0 seek=9 count=16 bs=128k
