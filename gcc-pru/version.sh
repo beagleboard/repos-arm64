0fd6e387e75b39de4d4ecc73f23ef7a178768d10
Robert Nelson <robertcnelson@gmail.com>
2022-05-10 16:29:37 -0500

    gcc-pru (12.1.0-0rcnee0)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- gcc-pru/version.sh
+++ gcc-pru/version.sh
@@ -2,7 +2,7 @@
 
 package_name="gcc-pru"
 debian_pkg_name="${package_name}"
-package_version="12.0.RC.gaeedb00a1a"
+package_version="12.1.0"
 package_source=""
 src_dir=""
 
@@ -13,9 +13,9 @@ git_sha=""
 reprepro_dir="b/${package_name}"
 dl_path="pool/main/${reprepro_dir}/"
 
-debian_version="${package_version}-0rcnee1"
+debian_version="${package_version}-0rcnee0"
 debian_untar=""
 debian_patch=""
 
-bullseye_version="~bullseye+20211223"
-focal_version="~focal+20211223"
+bullseye_version="~bullseye+20220510"
+focal_version="~focal+20220510"
