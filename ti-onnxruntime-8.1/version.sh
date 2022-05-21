a6057cb7f3facf588656740e55c8b32bc0216c6f
Robert Nelson <robertcnelson@gmail.com>
2022-05-21 09:09:18 -0500

    ti-onnxruntime-8.1 (8.1.0.21)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- ti-onnxruntime-8.1/version.sh
+++ ti-onnxruntime-8.1/version.sh
@@ -2,7 +2,7 @@
 
 package_name="ti-onnxruntime-8.1"
 debian_pkg_name="${package_name}"
-package_version="8.1.0.20"
+package_version="8.1.0.21"
 package_source=""
 src_dir=""
 
@@ -19,4 +19,4 @@ debian_dl_1="https://github.com/protocolbuffers/protobuf/releases/download/v3.11
 
 clear_changelog="enable"
 
-bullseye_version="~bullseye+20220520"
+bullseye_version="~bullseye+20220521"
