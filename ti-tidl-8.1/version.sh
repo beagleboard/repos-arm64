f060d91f48035dbd4e710dfd781f1349bfa8fb3f
Robert Nelson <robertcnelson@gmail.com>
2022-05-21 13:44:49 -0500

    ti-tidl-8.1 (8.1.0.4)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- ti-tidl-8.1/version.sh
+++ ti-tidl-8.1/version.sh
@@ -2,7 +2,7 @@
 
 package_name="ti-tidl-8.1"
 debian_pkg_name="${package_name}"
-package_version="8.1.0.3"
+package_version="8.1.0.4"
 package_source=""
 src_dir=""
 
@@ -17,4 +17,4 @@ debian_patch=""
 
 clear_changelog="enable"
 
-bullseye_version="~bullseye+20220520"
+bullseye_version="~bullseye+20220521"
