16fb731ca4e0dfd8d5fc8257bf6a2ca75b063eb8
Robert Nelson <robertcnelson@gmail.com>
2022-05-24 12:57:09 -0500

    ti-neo-ai-dlr-8.1 (8.1.0.15)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- ti-neo-ai-dlr-8.1/version.sh
+++ ti-neo-ai-dlr-8.1/version.sh
@@ -2,7 +2,7 @@
 
 package_name="ti-neo-ai-dlr-8.1"
 debian_pkg_name="${package_name}"
-package_version="8.1.0.14"
+package_version="8.1.0.15"
 package_source=""
 src_dir=""
 
@@ -17,4 +17,4 @@ debian_patch=""
 
 clear_changelog="enable"
 
-bullseye_version="~bullseye+20220523"
+bullseye_version="~bullseye+20220524"
