5f0fa782d0e7037ba0b6a61da89e9b798aa4694a
Robert Nelson <robertcnelson@gmail.com>
2022-05-24 11:42:25 -0500

    ti-dlpack-src-8.1 (8.1.0.0)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

---
+++ ti-dlpack-src-8.1/version.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -e
+
+package_name="ti-dlpack-src-8.1"
+debian_pkg_name="${package_name}"
+package_version="8.1.0.0"
+package_source=""
+src_dir=""
+
+git_repo=""
+git_sha=""
+reprepro_dir="t/${package_name}"
+dl_path="pool/main/${reprepro_dir}/"
+
+debian_version="${package_version}-0"
+debian_untar=""
+debian_patch=""
+
+clear_changelog="enable"
+
+bullseye_version="~bullseye+20220524"
