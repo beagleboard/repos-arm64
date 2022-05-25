7063fcd9682f84dae859b85f2ec642880c840572
Robert Nelson <robertcnelson@gmail.com>
2022-05-25 09:37:28 -0500

    ti-neo-ai-dlr-8.2 (8.2.0.0)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

---
+++ ti-neo-ai-dlr-8.2/version.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -e
+
+package_name="ti-neo-ai-dlr-8.2"
+debian_pkg_name="${package_name}"
+package_version="8.2.0.0"
+package_source=""
+src_dir=""
+
+git_repo=""
+git_sha=""
+reprepro_dir="t/${package_name}"
+dl_path="pool/main/${reprepro_dir}/"
+
+debian_version="${package_version}-0"
+debian_untar=""
+debian_patch=""
+
+clear_changelog="enable"
+
+bullseye_version="~bullseye+20220525"
