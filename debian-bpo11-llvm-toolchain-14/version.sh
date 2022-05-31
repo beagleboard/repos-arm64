6bc242d8606863df1fbd518312272bc2530f002d
Robert Nelson <robertcnelson@gmail.com>
2022-05-31 09:26:13 -0500

    backport: 14.0.4-2
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- debian-bpo11-llvm-toolchain-14/version.sh
+++ debian-bpo11-llvm-toolchain-14/version.sh
@@ -5,14 +5,14 @@ mirror="https://deb.debian.org/debian"
 
 package_name="llvm-toolchain-14"
 debian_pkg_name="${package_name}"
-package_version="14.0.3"
+package_version="14.0.4"
 
 git_repo=""
 git_sha=""
 reprepro_dir="l/${package_name}"
 dl_path="pool/main/${reprepro_dir}/"
 
-debian_version="${package_version}-1"
+debian_version="${package_version}-2"
 debian_untar="${package_name}_${debian_version}.debian.tar.xz"
 debian_patch=""
 
