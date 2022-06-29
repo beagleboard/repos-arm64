1c84a14b6e74d6bd7072832f51cd700f4bd881ac
Robert Nelson <robertcnelson@gmail.com>
2022-06-29 15:11:32 -0500

    ti-ew-2022 (1.20220616.3)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- ti-ew-2022/version.sh
+++ ti-ew-2022/version.sh
@@ -2,7 +2,7 @@
 
 package_name="ti-ew-2022"
 debian_pkg_name="${package_name}"
-package_version="1.20220616.2"
+package_version="1.20220616.3"
 package_source=""
 src_dir=""
 
@@ -17,4 +17,4 @@ debian_patch=""
 
 clear_changelog="enable"
 
-bullseye_version="~bullseye+20220616"
+bullseye_version="~bullseye+20220629"
