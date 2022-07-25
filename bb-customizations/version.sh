28d0841800767df583586c03c8d56729ee656a0e
Robert Nelson <robertcnelson@gmail.com>
2022-07-25 12:29:47 -0500

    bb-customizations (1.20220725.0)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- bb-customizations/version.sh
+++ bb-customizations/version.sh
@@ -4,7 +4,7 @@
 
 package_name="bb-customizations"
 debian_pkg_name="${package_name}"
-package_version="1.20220706.0"
+package_version="1.20220725.0"
 package_source=""
 src_dir=""
 
@@ -19,4 +19,4 @@ debian_patch=""
 
 clear_changelog="enable"
 
-bullseye_version="~bullseye+20220706"
+bullseye_version="~bullseye+20220725"
