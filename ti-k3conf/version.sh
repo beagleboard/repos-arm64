55e13e97293ae1c3b2b2637fea845cefe9b868cb
Robert Nelson <robertcnelson@gmail.com>
2022-07-27 16:20:39 -0500

    fix git package
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- ti-k3conf/version.sh
+++ ti-k3conf/version.sh
@@ -5,8 +5,8 @@
 package_name="ti-k3conf"
 debian_pkg_name="${package_name}"
 package_version="0.2.0-git20220622.0"
-package_source="${package_name}-${package_version}.orig.tar.xz"
-src_dir="${package_name}-${package_version}"
+package_source="${package_name}_${package_version}.orig.tar.xz"
+src_dir="${package_name}_${package_version}"
 
 git_repo="https://git.ti.com/git/k3conf/k3conf.git"
 git_sha="48cb8e8d7dc962229a9f318cd512b4ddeec5cdc5"
