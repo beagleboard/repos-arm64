ae8eb8c1af157e6fb24a6fca0d7a4749168e8842
Robert Nelson <robertcnelson@gmail.com>
2022-07-27 16:19:30 -0500

    ti-k3conf (0.2.0-git20220622.0)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- ti-k3conf/version.sh
+++ ti-k3conf/version.sh
@@ -4,12 +4,12 @@
 
 package_name="ti-k3conf"
 debian_pkg_name="${package_name}"
-package_version="0.2.0-git20201222.2"
+package_version="0.2.0-git20220622.0"
 package_source="${package_name}-${package_version}.orig.tar.xz"
 src_dir="${package_name}-${package_version}"
 
 git_repo="https://git.ti.com/git/k3conf/k3conf.git"
-git_sha="79f007cd462384ce22e750e9002b714878f56892"
+git_sha="48cb8e8d7dc962229a9f318cd512b4ddeec5cdc5"
 reprepro_dir="t/${package_name}"
 dl_path=""
 
@@ -17,4 +17,6 @@ debian_version="${package_version}-0"
 debian_patch=""
 debian_diff=""
 
-bullseye_version="~bullseye+20210904"
+clear_changelog="enable"
+
+bullseye_version="~bullseye+20220727"
