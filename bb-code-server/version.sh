c7ab84e6398a89801939fd2a49fde2bc4ffcfa38
Robert Nelson <robertcnelson@gmail.com>
2022-08-09 11:36:39 -0500

    vscode, disable npm break (test)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- bb-code-server/version.sh
+++ bb-code-server/version.sh
@@ -4,7 +4,7 @@
 
 package_name="bb-code-server"
 debian_pkg_name="${package_name}"
-package_version="4.5.1.0"
+package_version="4.5.1.1"
 package_source=""
 src_dir=""
 
@@ -23,4 +23,4 @@ debian_dl_1="https://github.com/coder/code-server/releases/download/v4.5.1/code-
 clear_changelog="enable"
 
 buster_version="~buster+20220720"
-bullseye_version="~bullseye+20220720"
+bullseye_version="~bullseye+20220809"
