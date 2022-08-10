2d62108cc314e87e9c78735d7d95a250f2f7959b
Robert Nelson <robertcnelson@gmail.com>
2022-08-10 08:45:06 -0500

    mmc-utils (0+git20220624.d7b343fd-1bbbio0)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- debian-11-mmc-utils/version.sh
+++ debian-11-mmc-utils/version.sh
@@ -4,7 +4,7 @@ mirror="http://http.debian.net/debian"
 
 package_name="mmc-utils"
 debian_pkg_name="${package_name}"
-package_version="0+git20191004.73d6c59"
+package_version="0+git20220624.d7b343fd"
 package_source="${package_name}_${package_version}.orig.tar.xz"
 src_dir="${package_name}-${package_version}"
 
@@ -16,6 +16,6 @@ dl_path="pool/main/${reprepro_dir}/"
 debian_version="${package_version}-1"
 debian_untar="${package_name}_${debian_version}.debian.tar.xz"
 debian_patch=""
-local_patch="rcnee0"
+local_patch="bbbio0"
 
-bullseye_version="~bullseye+20211201"
+bullseye_version="~bullseye+20220810"
