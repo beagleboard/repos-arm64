08babb2f479b44d0f797dfda1fcce6c4646c271a
Robert Nelson <robertcnelson@gmail.com>
2022-09-14 13:32:29 -0500

    fix distrbox
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- debian-11-distrobox/version.sh
+++ debian-11-distrobox/version.sh
@@ -12,7 +12,7 @@ src_dir="${package_name}-${package_version}"
 
 git_repo=""
 git_sha=""
-reprepro_dir="c/${package_name}"
+reprepro_dir="d/${package_name}"
 dl_path="pool/main/${reprepro_dir}/"
 
 debian_version="${package_version}-2"
