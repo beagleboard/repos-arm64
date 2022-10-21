ea6a48a3b52a72237e48f95122b132af16d9c48a
Robert Nelson <robertcnelson@gmail.com>
2022-10-21 10:33:46 -0500

    bb-u-boot-beagleboneai64-beta (2021.10.20221019.0)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- bb-u-boot-beagleboneai64-beta/version.sh
+++ bb-u-boot-beagleboneai64-beta/version.sh
@@ -1,10 +1,10 @@
 #!/bin/bash -e
 
-#https://git.beagleboard.org/beagleboard/u-boot/-/commits/v2021.01-ti-08.01.00.006/
+#https://github.com/rcn-ee/u-boot/commits/v2021.01-ti-08.04.01.005
 
 package_name="bb-u-boot-beagleboneai64-beta"
 debian_pkg_name="${package_name}"
-package_version="2021.10.20220921.0"
+package_version="2021.10.20221019.0"
 package_source=""
 src_dir=""
 
@@ -19,4 +19,4 @@ debian_patch=""
 
 clear_changelog="enable"
 
-bullseye_version="~bullseye+20220921"
+bullseye_version="~bullseye+20221021"
