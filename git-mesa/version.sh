bffe98c8afad1b20211f2067d0a9be4c881f6d61
Robert Nelson <robertcnelson@gmail.com>
2022-11-21 10:40:32 -0600

    mesa (22.2.4-1.git20221116.0)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- git-mesa/version.sh
+++ git-mesa/version.sh
@@ -5,12 +5,12 @@ mirror="http://http.debian.net/debian"
 
 package_name="mesa"
 debian_pkg_name="${package_name}"
-package_version="22.2.3-1.git20221107.0"
+package_version="22.2.4-1.git20221116.0"
 package_source="${debian_pkg_name}_${package_version}.orig.tar.xz"
 src_dir="${package_name}_${package_version}"
 
 git_repo="https://gitlab.freedesktop.org/mesa/mesa"
-git_sha="ce33e68fe0a041cbd1794713402e8e53d2470531"
+git_sha="a21e3efe21ef02708bc060262ee91f18a366cc44"
 reprepro_dir="m/${package_name}"
 dl_path=""
 
@@ -18,4 +18,4 @@ debian_version="${package_version}-0"
 debian_patch=""
 debian_diff=""
 
-bullseye_version="~bullseye+20221116"
+bullseye_version="~bullseye+20221121"
