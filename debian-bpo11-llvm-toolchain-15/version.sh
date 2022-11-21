08114fbb0ddd970f2fb9250dadf466031578eeba
Robert Nelson <robertcnelson@gmail.com>
2022-11-21 17:00:27 +0000

    llvm-toolchain-15 (1:15.0.5-3)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- debian-bpo11-llvm-toolchain-15/version.sh
+++ debian-bpo11-llvm-toolchain-15/version.sh
@@ -5,14 +5,14 @@ mirror="https://deb.debian.org/debian"
 
 package_name="llvm-toolchain-15"
 debian_pkg_name="${package_name}"
-package_version="15.0.2"
+package_version="15.0.5"
 
 git_repo=""
 git_sha=""
 reprepro_dir="l/${package_name}"
 dl_path="pool/main/${reprepro_dir}/"
 
-debian_version="${package_version}-1"
+debian_version="${package_version}-3"
 debian_untar="${package_name}_${debian_version}.debian.tar.xz"
 debian_patch=""
 
