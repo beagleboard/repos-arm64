8fd8cab2d4308c7ab652a7531f9d2bc5183943b4
Robert Nelson <robertcnelson@gmail.com>
2022-11-26 13:24:45 -0600

    linuxcnc (2.9.0~pre0+git20221105.ffb6bda926-1.2bbbio2)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- debian-11-linuxcnc/version.sh
+++ debian-11-linuxcnc/version.sh
@@ -1,14 +1,14 @@
 #!/bin/bash -e
 
 #https://tracker.debian.org/pkg/linuxcnc
-#https://packages.debian.org/source/testing/linuxcnc
+#https://packages.debian.org/source/sid/linuxcnc
 
 mirror="http://http.debian.net/debian"
 
 package_name="linuxcnc"
 debian_pkg_name="${package_name}"
-package_version="2.9.0~pre0+git20220906.02744cdef6"
-package_source="${debian_pkg_name}_${package_version}.orig.tar.gz"
+package_version="2.9.0~pre0+git20221105.ffb6bda926"
+package_source="${debian_pkg_name}_${package_version}.orig.tar.xz"
 src_dir="${package_name}-${package_version}"
 
 git_repo=""
@@ -16,9 +16,9 @@ git_sha=""
 reprepro_dir="l/${package_name}"
 dl_path="pool/main/${reprepro_dir}/"
 
-debian_version="${package_version}-1"
+debian_version="${package_version}-1.2"
 debian_untar="${package_name}_${debian_version}.debian.tar.xz"
 debian_patch=""
-local_patch="bbbio0"
+local_patch="bbbio2"
 
-bullseye_version="~bullseye+20220914"
+bullseye_version="~bullseye+20221126"
