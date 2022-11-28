7b21b2e437b6e4a9fe68911b0120684edc12f863
Robert Nelson <robertcnelson@gmail.com>
2022-11-28 11:19:48 -0600

    spirv-llvm-translator-15 (15.0.0-1bbbio2)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- debian-11-spirv-llvm-translator-15/version.sh
+++ debian-11-spirv-llvm-translator-15/version.sh
@@ -17,6 +17,6 @@ dl_path="pool/main/${reprepro_dir}/"
 debian_version="${package_version}-1"
 debian_untar="${package_name}_${debian_version}.debian.tar.xz"
 debian_patch=""
-local_patch="bbbio1"
+local_patch="bbbio2"
 
 bullseye_version="~bullseye+20221128"
