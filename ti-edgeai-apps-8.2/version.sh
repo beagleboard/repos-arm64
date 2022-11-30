a59fd7ebb55200f7eeb9b5b8f2830203d3955ea8
Robert Nelson <robertcnelson@gmail.com>
2022-11-30 12:08:07 -0600

    ti-edgeai-apps-8.2 (8.2.0.11-0)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- ti-edgeai-apps-8.2/version.sh
+++ ti-edgeai-apps-8.2/version.sh
@@ -2,7 +2,7 @@
 
 package_name="ti-edgeai-apps-8.2"
 debian_pkg_name="${package_name}"
-package_version="8.2.0.10"
+package_version="8.2.0.11"
 package_source=""
 src_dir=""
 
@@ -17,4 +17,4 @@ debian_patch=""
 
 clear_changelog="enable"
 
-bullseye_version="~bullseye+20220527"
+bullseye_version="~bullseye+20221130"
