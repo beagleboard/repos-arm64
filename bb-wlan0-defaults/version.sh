7846808775fda496b7f781eb90abe60b431c0d17
Robert Nelson <robertcnelson@gmail.com>
2022-12-08 08:28:49 -0600

    bb-wlan0-defaults (1.20221208.0)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- bb-wlan0-defaults/version.sh
+++ bb-wlan0-defaults/version.sh
@@ -2,7 +2,7 @@
 
 package_name="bb-wlan0-defaults"
 debian_pkg_name="${package_name}"
-package_version="1.20220103.1"
+package_version="1.20221208.0"
 package_source=""
 src_dir=""
 
@@ -17,4 +17,4 @@ debian_patch=""
 
 clear_changelog="enable"
 
-bullseye_version="~bullseye+20220103"
+bullseye_version="~bullseye+20221208"
