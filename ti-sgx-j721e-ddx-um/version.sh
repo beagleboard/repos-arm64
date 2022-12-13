1b92b3c6c619bfa6bf9186a79c5063136f75af2c
Robert Nelson <robertcnelson@gmail.com>
2022-12-13 00:15:32 -0600

    bb-u-boot-sk-am62 (2021.01.20221213.0)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- ti-sgx-j721e-ddx-um/version.sh
+++ ti-sgx-j721e-ddx-um/version.sh
@@ -4,7 +4,7 @@
 
 package_name="ti-sgx-j721e-ddx-um"
 debian_pkg_name="${package_name}"
-package_version="1.15.6133109-k5.10-git20221011.0"
+package_version="1.15.6133109-k5.10-git20221011.1"
 package_source="${package_name}_${package_version}.orig.tar.xz"
 src_dir="${package_name}_${package_version}"
 
