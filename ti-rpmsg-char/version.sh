6aaea1569993539031ec30a3f2e2e7e61b8629f4
Robert Nelson <robertcnelson@gmail.com>
2023-01-09 17:41:13 -0600

    ti-rpmsg-char (0.5.2.20221220.0)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- ti-rpmsg-char/version.sh
+++ ti-rpmsg-char/version.sh
@@ -4,12 +4,12 @@
 
 package_name="ti-rpmsg-char"
 debian_pkg_name="${package_name}"
-package_version="0.4.1.20220222.1"
+package_version="0.5.2.20221220.0"
 package_source="${package_name}_${package_version}.orig.tar.xz"
 src_dir="${package_name}_${package_version}"
 
 git_repo="https://git.ti.com/git/rpmsg/ti-rpmsg-char.git"
-git_sha="355d2b0bcde5eb5c895a270d1835102d2bc39fbd"
+git_sha="1da3e1bd0412c148ab57690940fea5d129220896"
 reprepro_dir="t/${package_name}"
 dl_path=""
 
@@ -19,5 +19,4 @@ debian_diff=""
 
 clear_changelog="enable"
 
-bullseye_version="~bullseye+20220405"
-focal_version="~focal+20220405"
+bullseye_version="~bullseye+20230109"
