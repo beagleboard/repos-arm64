273a0a9339eca32070a28163570c20f5a3351c38
Robert Nelson <robertcnelson@gmail.com>
2023-02-02 14:22:57 -0600

    beagleconnect (1.20230202.0)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- bbb.io-beagleconnect/version.sh
+++ bbb.io-beagleconnect/version.sh
@@ -2,7 +2,7 @@
 
 package_name="beagleconnect"
 debian_pkg_name="${package_name}"
-package_version="1.20211217.1"
+package_version="1.20230202.0"
 package_source=""
 src_dir=""
 
@@ -15,7 +15,9 @@ debian_version="${package_version}-0"
 debian_untar=""
 debian_patch=""
 
+debian_dl_1="https://debian.beagle.cc/images/cc1352-wpanusb-0.0.2.zip"
+
 clear_changelog="enable"
 
 buster_version="~buster+20211217"
-bullseye_version="~bullseye+20211217"
+bullseye_version="~bullseye+20230202"
