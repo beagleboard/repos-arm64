8c88f5d3d5e3539f6930e213b1141a1ae3992aa6
Robert Nelson <robertcnelson@gmail.com>
2023-02-02 20:41:14 -0600

    warpinator (1.4.4-0bbbio0)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- git-warpinator/version.sh
+++ git-warpinator/version.sh
@@ -4,12 +4,12 @@
 
 package_name="warpinator"
 debian_pkg_name="${package_name}"
-package_version="1.4.3"
+package_version="1.4.4"
 package_source="${package_name}_${package_version}.orig.tar.xz"
 src_dir="${package_name}_${package_version}"
 
 git_repo="https://github.com/linuxmint/warpinator"
-git_sha="1773127d1f7fd5fca20610ed655df104e25177a4"
+git_sha="34b18a19e4208da517668bf11f09c9e562f6ccc3"
 reprepro_dir="w/${package_name}"
 dl_path=""
 
@@ -18,4 +18,4 @@ debian_patch=""
 debian_diff=""
 local_patch="bbbio0"
 
-bullseye_version="~bullseye+20230106"
+bullseye_version="~bullseye+20230202"
