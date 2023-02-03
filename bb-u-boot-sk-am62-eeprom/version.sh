e9db25dde6c427cbde8f9dc1be58ef52fdf64a46
Robert Nelson <robertcnelson@gmail.com>
2023-02-02 20:22:31 -0600

    bb-u-boot-sk-am62-eeprom (2021.01.20230104.1)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- bb-u-boot-sk-am62-eeprom/version.sh
+++ bb-u-boot-sk-am62-eeprom/version.sh
@@ -4,7 +4,7 @@
 
 package_name="bb-u-boot-sk-am62-eeprom"
 debian_pkg_name="${package_name}"
-package_version="2021.01.20230104.0"
+package_version="2021.01.20230104.1"
 package_source=""
 src_dir=""
 
