0ffcf83c4a458ed9d660253d973c5efbd42fcf17
Robert Nelson <robertcnelson@gmail.com>
2023-02-13 11:52:33 -0600

    bb-u-boot-sk-am62-next (2021.01.20230213.2)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- bb-u-boot-sk-am62-next/version.sh
+++ bb-u-boot-sk-am62-next/version.sh
@@ -4,7 +4,7 @@
 
 package_name="bb-u-boot-sk-am62-next"
 debian_pkg_name="${package_name}"
-package_version="2021.01.20230213.1"
+package_version="2021.01.20230213.2"
 package_source=""
 src_dir=""
 
