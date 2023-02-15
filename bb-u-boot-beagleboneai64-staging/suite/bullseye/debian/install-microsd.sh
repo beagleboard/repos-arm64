7fcb518c031425bfcd29294791597600fb05dcf8
Robert Nelson <robertcnelson@gmail.com>
2023-02-15 09:44:36 -0600

    bb-u-boot-beagleboneai64-staging (2021.10.20230213.0)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

---
+++ bb-u-boot-beagleboneai64-staging/suite/bullseye/debian/install-microsd.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+if ! id | grep -q root; then
+	echo "must be run as root"
+	exit
+fi
+
+wdir="/opt/u-boot/bb-u-boot-beagleboneai64-staging"
+
+if [ -d /boot/firmware/ ] ; then
+	cp -v /opt/u-boot/bb-k3-image-gen-j721e-evm/sysfw.itb /boot/firmware/
+	cp -v ${wdir}/tiboot3.bin /boot/firmware/
+	cp -v ${wdir}/tispl.bin /boot/firmware/
+	cp -v ${wdir}/u-boot.img /boot/firmware/
+fi
