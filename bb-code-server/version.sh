a3e79f464d02e44e010e94deefcc13c664e57fc7
Robert Nelson <robertcnelson@gmail.com>
2023-02-28 16:31:34 -0600

    bb-code-server (4.10.0)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- bb-code-server/version.sh
+++ bb-code-server/version.sh
@@ -1,9 +1,9 @@
 #!/bin/bash -e
 
-#https://github.com/coder/code-server/releases/tag/v4.9.0
+#https://github.com/coder/code-server/releases/tag/v4.10.0
 
 arch="arm64"
-ver="4.9.1"
+ver="4.10.0"
 
 package_name="bb-code-server"
 debian_pkg_name="${package_name}"
@@ -24,4 +24,4 @@ debian_dl_1="https://github.com/coder/code-server/releases/download/v${ver}/code
 
 clear_changelog="enable"
 
-bullseye_version="~bullseye+20221220"
+bullseye_version="~bullseye+20230228"
