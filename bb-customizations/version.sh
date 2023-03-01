124534ad5f7d45cab6a063eeded5a20ddc7a1d9e
Robert Nelson <robertcnelson@gmail.com>
2023-03-01 14:10:11 -0600

    bb-customizations (1.20230301.0) [Include 0-005* range for possible eeproms]
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- bb-customizations/version.sh
+++ bb-customizations/version.sh
@@ -4,7 +4,7 @@
 
 package_name="bb-customizations"
 debian_pkg_name="${package_name}"
-package_version="1.20221230.0"
+package_version="1.20230301.0"
 package_source=""
 src_dir=""
 
@@ -19,4 +19,4 @@ debian_patch=""
 
 clear_changelog="enable"
 
-bullseye_version="~bullseye+20221230"
+bullseye_version="~bullseye+20230301"
