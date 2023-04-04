045182a4c60fab70028bff7d424ae688ba314b6b
Robert Nelson <robertcnelson@gmail.com>
2023-04-04 12:42:10 -0500

    ti-sgx-j721e-ddx-um (1.15.6133109-k5.10-git20221130.1)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- ti-sgx-j721e-ddx-um/version.sh
+++ ti-sgx-j721e-ddx-um/version.sh
@@ -4,7 +4,7 @@
 
 package_name="ti-sgx-j721e-ddx-um"
 debian_pkg_name="${package_name}"
-package_version="1.15.6133109-k5.10-git20221130.0"
+package_version="1.15.6133109-k5.10-git20221130.1"
 package_source=""
 src_dir=""
 
