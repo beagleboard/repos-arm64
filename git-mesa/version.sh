5fce9654cbeff87b895bc29e208a254a8dd97c8f
Robert Nelson <robertcnelson@gmail.com>
2023-04-04 15:52:41 -0500

    mesa (22.3.5-1.git20230331.0)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- git-mesa/version.sh
+++ git-mesa/version.sh
@@ -1,16 +1,17 @@
 #!/bin/bash -e
 
 #https://gitlab.freedesktop.org/mesa/mesa/-/commits/22.3/
+#https://gitlab.freedesktop.org/StaticRocket/mesa/-/tree/powervr/kirkstone/22.3.5
 mirror="http://http.debian.net/debian"
 
 package_name="mesa"
 debian_pkg_name="${package_name}"
-package_version="22.3.5-1.git20230208.0"
+package_version="22.3.5-1.git20230331.0"
 package_source="${debian_pkg_name}_${package_version}.orig.tar.xz"
 src_dir="${package_name}_${package_version}"
 
-git_repo="https://gitlab.freedesktop.org/mesa/mesa"
-git_sha="6570a15662e7b174080ef62335bb226eec84a6e8"
+git_repo="https://gitlab.freedesktop.org/StaticRocket/mesa"
+git_sha="54fd9d7dea098b6f11c2a244b0c6763dc8c5690c"
 reprepro_dir="m/${package_name}"
 dl_path=""
 
