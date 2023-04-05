1167638c768282bb412d9636830480d77127c3ab
Robert Nelson <robertcnelson@gmail.com>
2023-04-05 15:35:12 -0500

    bb-u-boot-beagleboneai64 (2021.10.20230216.1)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

---
+++ bb-u-boot-beagleboneai64/suite/bookworm/debian/convert-to-extlinux-emmc.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if ! id | grep -q root; then
+	echo "must be run as root"
+	exit
+fi
+
+wdir="/opt/u-boot/bb-u-boot-beagleboneai64"
+
+if [ -d /boot/firmware/ ] ; then
+	mkdir -p /boot/firmware/extlinux/
+	cp -v ${wdir}/emmc-extlinux.conf /boot/firmware/extlinux/extlinux.conf
+	cp -v /boot/Image-`uname -r` /boot/firmware/Image
+	cp -v /boot/initrd.img-`uname -r` /boot/firmware/initrd.img
+	cp -v /boot/dtbs/`uname -r`/ti/k3-j721e-*dtb /boot/firmware/
+fi
