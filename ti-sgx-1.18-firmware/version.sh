4ff4e0558c1043c91c5786e38c96c7779f88027a
Robert Nelson <robertcnelson@gmail.com>
2023-04-05 16:08:55 -0500

    ti-sgx-1.18-firmware (1.18.6276027-git20221213.0)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

---
+++ ti-sgx-1.18-firmware/version.sh
@@ -0,0 +1,22 @@
+#!/bin/bash -e
+
+#https://git.ti.com/gitweb?p=graphics/ti-img-rogue-umlibs.git;a=summary
+
+package_name="ti-sgx-1.18-firmware"
+debian_pkg_name="${package_name}"
+package_version="1.18.6276027-git20221213.0"
+package_source=""
+src_dir=""
+
+git_repo=""
+git_sha=""
+reprepro_dir="t/${package_name}"
+dl_path=""
+
+debian_version="${package_version}-0"
+debian_patch=""
+debian_diff=""
+
+clear_changelog="enable"
+
+bookworm_version="~bookworm+20230405"
