273ec0943e050012b8e3e14bd0a572b420b4a2a1
Robert Nelson <robertcnelson@gmail.com>
2023-04-05 16:00:48 -0500

    fixes
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- ti-devmem2/version.sh
+++ ti-devmem2/version.sh
@@ -5,7 +5,7 @@
 package_name="ti-devmem2"
 debian_pkg_name="${package_name}"
 package_version="0.0.0-git20210904.3"
-package_source="${package_name}-${package_version}.orig.tar.xz"
+package_source="${package_name}_${package_version}.orig.tar.xz"
 src_dir="${package_name}_${package_version}"
 
 git_repo="https://github.com/rcn-ee/devmem2"
