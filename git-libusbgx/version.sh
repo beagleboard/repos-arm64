cb09287ea21366b39ffcbab1dd5fdf3d725b7a58
Robert Nelson <robertcnelson@gmail.com>
2023-04-05 16:15:10 -0500

    fix
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- git-libusbgx/version.sh
+++ git-libusbgx/version.sh
@@ -5,8 +5,8 @@
 package_name="libusbgx"
 debian_pkg_name="${package_name}"
 package_version="0.2.0-git20210811.2"
-package_source="${package_name}-${package_version}.orig.tar.xz"
-src_dir="${package_name}-${package_version}"
+package_source="${package_name}_${package_version}.orig.tar.xz"
+src_dir="${package_name}_${package_version}"
 
 git_repo="https://github.com/linux-usb-gadgets/libusbgx"
 git_sha="fa4e7e92260e33f78b72e7796b3df28c4c84b8e2"
