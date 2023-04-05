6ebfddce75c160c8bcba54612f5a169565f0b242
Robert Nelson <robertcnelson@gmail.com>
2023-04-05 09:20:58 -0500

    bbb.io-getting-started (1.20230405.0)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- bbb.io-getting-started/version.sh
+++ bbb.io-getting-started/version.sh
@@ -2,7 +2,7 @@
 
 package_name="bbb.io-getting-started"
 debian_pkg_name="${package_name}"
-package_version="1.20230216.0"
+package_version="1.20230405.0"
 package_source=""
 src_dir=""
 
@@ -15,8 +15,8 @@ debian_version="${package_version}-0"
 debian_untar=""
 debian_patch=""
 
-debian_dl_1="https://git.beagleboard.org/beagleboard/getting-started-archive/-/raw/main/202302016/beagle-getting-started.img"
+debian_dl_1="https://git.beagleboard.org/beagleboard/getting-started-archive/-/raw/main/20230405/beagle-getting-started.img"
 
 clear_changelog="enable"
 
-bullseye_version="~bullseye+20230216"
+bullseye_version="~bullseye+20230405"
