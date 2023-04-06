7aae9e2dfecdf33c254422e544c6202103ebedea
Robert Nelson <robertcnelson@gmail.com>
2023-04-05 20:10:23 -0500

    ti-sgx-1.18-firmware (1.18.6276027-git20221213.2)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- ti-sgx-1.18-firmware/version.sh
+++ ti-sgx-1.18-firmware/version.sh
@@ -4,7 +4,7 @@
 
 package_name="ti-sgx-1.18-firmware"
 debian_pkg_name="${package_name}"
-package_version="1.18.6276027-git20221213.1"
+package_version="1.18.6276027-git20221213.2"
 package_source=""
 src_dir=""
 
@@ -19,4 +19,5 @@ debian_diff=""
 
 clear_changelog="enable"
 
+bullseye_version="~bullseye+20230405"
 bookworm_version="~bookworm+20230405"
