b55e3b217ba3050dc6702a3beaff68c83d0a4438
Robert Nelson <robertcnelson@gmail.com>
2023-04-14 15:59:43 -0500

    ti-onnxruntime-src-8.6 (8.6.0.0)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

---
+++ ti-onnxruntime-src-8.6/version.sh
@@ -0,0 +1,22 @@
+#!/bin/bash -e
+
+package_name="ti-onnxruntime-src-8.6"
+debian_pkg_name="${package_name}"
+package_version="8.6.0.0"
+package_source=""
+src_dir=""
+
+git_repo=""
+git_sha=""
+reprepro_dir="t/${package_name}"
+dl_path="pool/main/${reprepro_dir}/"
+
+debian_version="${package_version}-0"
+debian_untar=""
+debian_patch=""
+
+debian_dl_1="https://github.com/protocolbuffers/protobuf/releases/download/v3.11.3/protoc-3.11.3-linux-aarch_64.zip"
+
+clear_changelog="enable"
+
+bullseye_version="~bullseye+20230414"
