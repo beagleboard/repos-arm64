6c8ffc1d5b3b33cd2c2e5ea92c2c890b29e1c35a
Robert Nelson <robertcnelson@gmail.com>
2023-04-17 10:50:15 -0500

    ti-vision-apps-eaik-firmware-8.5 (8.5.0.0)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

---
+++ ti-vision-apps-eaik-firmware-8.5/version.sh
@@ -0,0 +1,24 @@
+#!/bin/bash -e
+
+#https://git.beagleboard.org/beagleboard/tisdk
+
+package_name="ti-vision-apps-eaik-firmware-8.5"
+debian_pkg_name="${package_name}"
+package_version="8.5.0.0"
+package_source=""
+src_dir=""
+
+git_repo=""
+git_sha=""
+reprepro_dir="t/${package_name}"
+dl_path="pool/main/${reprepro_dir}/"
+
+debian_version="${package_version}-0"
+debian_untar=""
+debian_patch=""
+
+debian_dl_1="https://github.com/rcn-ee/ti-edge-ai-bins/raw/main/8.5/firmware_08_05_00_11.tar.xz"
+
+clear_changelog="enable"
+
+bullseye_version="~bullseye+20230417"
