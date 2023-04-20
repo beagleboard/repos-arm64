923c089ae2b045b6676778ed6cda92c83ec35586
Robert Nelson <robertcnelson@gmail.com>
2023-04-20 14:00:38 -0500

    mesa (22.3.5-1.git20230331.1) - enable pvr vulkan
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- pvr-23.1-mesa/version.sh
+++ pvr-23.1-mesa/version.sh
@@ -5,7 +5,7 @@ mirror="http://http.debian.net/debian"
 
 package_name="mesa"
 debian_pkg_name="${package_name}"
-package_version="22.3.5-1.git20230331.0"
+package_version="22.3.5-1.git20230331.1"
 package_source="${debian_pkg_name}_${package_version}.orig.tar.xz"
 src_dir="${package_name}_${package_version}"
 
@@ -18,4 +18,4 @@ debian_version="${package_version}-0"
 debian_patch=""
 debian_diff=""
 
-bookworm_version="~bookworm+20230407"
+bookworm_version="~bookworm+20230420"
