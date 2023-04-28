9ca6f005b00203ffea476db1e510f480f4c84020
Robert Nelson <robertcnelson@gmail.com>
2023-04-28 09:04:21 -0500

    ti-sgx-23.1-j721e-ddx-um (23.1.6404501-git20230424.1)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- ti-sgx-23.1-j721e-ddx-um/version.sh
+++ ti-sgx-23.1-j721e-ddx-um/version.sh
@@ -2,7 +2,7 @@
 
 #https://git.ti.com/gitweb?p=graphics/ti-img-rogue-umlibs.git;a=summary
 
-package_name="ti-sgx-23.1-am62-ddx-um"
+package_name="ti-sgx-23.1-j721e-ddx-um"
 debian_pkg_name="${package_name}"
 package_version="23.1.6404501-git20230424.1"
 package_source=""
@@ -19,4 +19,4 @@ debian_diff=""
 
 clear_changelog="enable"
 
-bookworm_version="~bookworm+20230425"
+bookworm_version="~bookworm+20230428"
