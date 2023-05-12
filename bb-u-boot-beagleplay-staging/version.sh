59df747a84be552360215bebdc4fc9557d5d9aee
Robert Nelson <robertcnelson@gmail.com>
2023-05-12 15:43:50 -0500

    bb-u-boot-beagleplay-staging (2021.01.20230213.3)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

---
+++ bb-u-boot-beagleplay-staging/version.sh
@@ -0,0 +1,23 @@
+#!/bin/bash -e
+
+#https://git.beagleboard.org/beagleboard/u-boot/
+
+package_name="bb-u-boot-beagleplay-staging"
+debian_pkg_name="${package_name}"
+package_version="2021.01.20230213.3"
+package_source=""
+src_dir=""
+
+git_repo=""
+git_sha=""
+reprepro_dir="b/${package_name}"
+dl_path="pool/main/${reprepro_dir}/"
+
+debian_version="${package_version}-0"
+debian_untar=""
+debian_patch=""
+
+clear_changelog="enable"
+
+bullseye_version="~bullseye+20230512"
+bookworm_version="~bookworm+20230512"
