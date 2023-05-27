429498ae85b46aef3d647ce861c55ab50238b352
Robert Nelson <robertcnelson@gmail.com>
2023-05-27 12:04:22 -0500

    bbb.io-kernel (1.20230527.0)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- bbb.io-kernel/update.sh
+++ bbb.io-kernel/update.sh
@@ -113,6 +113,13 @@ do_bullseye () {
 	sgxmodule="sgx"
 
 	msg="5.10-ti"   ; var="ti-arm64" ; ver="LTS510"  ; current_kernel ; generate_kernel_ti
+
+	unset sgxam62
+	unset sgxj721e
+	unset sgxmodule
+
+	msg="6.1-ti"    ; var="ti-arm64" ; ver="LTS61"   ; current_kernel ; generate_kernel_ti
+
 	msg="mainline"  ; var="arm64"    ; ver="STABLE"  ; current_kernel ; generate_mainline_kernel
 	msg="6.3-k3"    ; var="k3-arm64"    ; ver="V63X" ; current_kernel ; generate_kernel_k3
 	msg="6.3-rt-k3" ; var="k3-arm64-rt" ; ver="V63X" ; current_kernel ; generate_kernel_k3
@@ -132,6 +139,13 @@ do_bookworm () {
 	sgxmodule="sgx-23.1"
 
 	msg="5.10-ti"   ; var="ti-arm64" ; ver="LTS510"  ; current_kernel ; generate_kernel_ti
+
+	unset sgxam62
+	unset sgxj721e
+	unset sgxmodule
+
+	msg="6.1-ti"    ; var="ti-arm64" ; ver="LTS61"   ; current_kernel ; generate_kernel_ti
+
 	msg="mainline"  ; var="arm64"    ; ver="STABLE"  ; current_kernel ; generate_mainline_kernel
 	msg="6.3-k3"    ; var="k3-arm64"    ; ver="V63X" ; current_kernel ; generate_kernel_k3
 	msg="6.3-rt-k3" ; var="k3-arm64-rt" ; ver="V63X" ; current_kernel ; generate_kernel_k3
