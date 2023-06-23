2a4d77c5a8f1754f4fb887f6d38b4ea4d88d5da7
Robert Nelson <robertcnelson@gmail.com>
2023-06-23 09:31:52 -0500

    bb-beagle-version (1.20230623.0)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- bb-beagle-version/version.sh
+++ bb-beagle-version/version.sh
@@ -4,7 +4,7 @@
 
 package_name="bb-beagle-version"
 debian_pkg_name="${package_name}"
-package_version="1.20230515.0"
+package_version="1.20230623.0"
 package_source=""
 src_dir=""
 
@@ -19,5 +19,7 @@ debian_patch=""
 
 clear_changelog="enable"
 
-bullseye_version="~bullseye+20230515"
-bookworm_version="~bookworm+20230515"
+bullseye_version="~bullseye+20230623"
+bookworm_version="~bookworm+20230623"
+trixie_version="~trixie+20230623"
+jammy_version="~jammy+20230623"
