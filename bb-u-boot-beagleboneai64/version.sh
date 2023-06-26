25cf81185c5570d0c58e8205dafbfa1154c903d3
Robert Nelson <robertcnelson@gmail.com>
2023-06-26 09:48:55 -0500

    bb-u-boot-beagleboneai64 (2021.10.20230216.4)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- bb-u-boot-beagleboneai64/version.sh
+++ bb-u-boot-beagleboneai64/version.sh
@@ -4,7 +4,7 @@
 
 package_name="bb-u-boot-beagleboneai64"
 debian_pkg_name="${package_name}"
-package_version="2021.10.20230216.3"
+package_version="2021.10.20230216.4"
 package_source=""
 src_dir=""
 
@@ -19,7 +19,7 @@ debian_patch=""
 
 clear_changelog="enable"
 
-bullseye_version="~bullseye+20230623"
-bookworm_version="~bookworm+20230623"
-trixie_version="~trixie+20230623"
-jammy_version="~jammy+20230623"
+bullseye_version="~bullseye+20230626"
+bookworm_version="~bookworm+20230626"
+trixie_version="~trixie+20230626"
+jammy_version="~jammy+20230626"
