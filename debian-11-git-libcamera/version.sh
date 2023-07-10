196ec4b03a8e02565508cc428d9ccce2ba8f3e72
Robert Nelson <robertcnelson@gmail.com>
2023-07-10 16:04:46 -0500

    libcamera (0.0.5-2bbbio1)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- debian-11-git-libcamera/version.sh
+++ debian-11-git-libcamera/version.sh
@@ -18,7 +18,7 @@ dl_path="pool/main/${reprepro_dir}/"
 debian_version="${package_version}-2"
 debian_untar="${package_name}_${debian_version}.debian.tar.xz"
 debian_patch=""
-local_patch="bbbio0"
+local_patch="bbbio1"
 
 bullseye_version="~bullseye+20230710"
 bookworm_version="~bookworm+20230710"
