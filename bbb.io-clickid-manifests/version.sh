eb285b7134dd29f4044a28bb88513032effa0fb9
Robert Nelson <robertcnelson@gmail.com>
2023-07-13 15:01:57 -0500

    bbb.io-clickid-manifests (1.20230713.0)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- bbb.io-clickid-manifests/version.sh
+++ bbb.io-clickid-manifests/version.sh
@@ -2,12 +2,12 @@
 
 package_name="bbb.io-clickid-manifests"
 debian_pkg_name="${package_name}"
-package_version="1.20230616.5"
+package_version="1.20230713.0"
 package_source="${package_name}_${package_version}.orig.tar.xz"
 src_dir="${package_name}_${package_version}"
 
 git_repo="https://git.beagleboard.org/beagleconnect/manifesto.git"
-git_sha="d937e007d09e16aa53e8f7407f9126acd9e711c8"
+git_sha="3acef3c45dd107c39d1c09ba24e8ea0b7d72572b"
 reprepro_dir="b/${package_name}"
 dl_path=""
 
@@ -17,7 +17,7 @@ debian_diff=""
 
 clear_changelog="enable"
 
-bullseye_version="~bullseye+20230623"
-bookworm_version="~bookworm+20230623"
-trixie_version="~trixie+20230623"
-jammy_version="~jammy+20230623"
+bullseye_version="~bullseye+20230713"
+bookworm_version="~bookworm+20230713"
+trixie_version="~trixie+20230713"
+jammy_version="~jammy+20230713"
