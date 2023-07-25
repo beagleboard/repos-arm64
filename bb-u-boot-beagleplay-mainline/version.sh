74f9be3fe697145441b96caed707a0f224c8435a
Robert Nelson <robertcnelson@gmail.com>
2023-07-25 15:48:51 -0500

    bb-u-boot-beagleplay-mainline (2023.07.20230725.0)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

---
+++ bb-u-boot-beagleplay-mainline/version.sh
@@ -0,0 +1,22 @@
+#!/bin/bash -e
+
+#https://git.beagleboard.org/beagleboard/u-boot/
+
+package_name="bb-u-boot-beagleplay-mainline"
+debian_pkg_name="${package_name}"
+package_version="2023.07.20230725.0"
+package_source=""
+src_dir=""
+
+git_repo=""
+git_sha=""
+reprepro_dir="b/${package_name}"
+dl_path="pool/main/${reprepro_dir}/"
+
+debian_version="${package_version}-0"
+debian_untar=""
+debian_patch=""
+
+clear_changelog="enable"
+
+bullseye_version="~bullseye+20230725"
