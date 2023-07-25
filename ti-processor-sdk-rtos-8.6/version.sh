1bd0764d7b01b2c0284cd7d820fb0deffe7be79c
Robert Nelson <robertcnelson@gmail.com>
2023-07-25 11:25:28 -0500

    ti-processor-sdk-rtos-8.6 (8.6.1.3)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- ti-processor-sdk-rtos-8.6/version.sh
+++ ti-processor-sdk-rtos-8.6/version.sh
@@ -2,7 +2,7 @@
 
 package_name="ti-processor-sdk-rtos-8.6"
 debian_pkg_name="${package_name}"
-package_version="8.6.0.1"
+package_version="8.6.1.3"
 package_source=""
 src_dir=""
 
@@ -15,9 +15,9 @@ debian_version="${package_version}-0"
 debian_untar=""
 debian_patch=""
 
-debian_dl_1="http://192.168.1.98/internal/dl/files/ti-processor-sdk-rtos-j721e-evm-08_06_00_12.tar.gz"
+debian_dl_1="http://192.168.1.98/internal/dl/files/ti-processor-sdk-rtos-j721e-evm-08_06_01_03.tar.gz"
 debian_dl_2="https://git.yoctoproject.org/cgit/cgit.cgi/opkg-utils/snapshot/opkg-utils-master.tar.gz"
 
 clear_changelog="enable"
 
-bullseye_version="~bullseye+20230413"
+bullseye_version="~bullseye+20230725"
