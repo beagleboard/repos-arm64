4db0495af9a401e63b96c612b68be0530a2d1e68
Robert Nelson <robertcnelson@gmail.com>
2023-07-25 19:11:16 -0500

    bb-u-boot-beagleplay-mainline (2023.07.20230725.5) - drop bullseye build failure in u-boot with gcc
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- bb-u-boot-beagleplay-mainline/version.sh
+++ bb-u-boot-beagleplay-mainline/version.sh
@@ -4,7 +4,7 @@
 
 package_name="bb-u-boot-beagleplay-mainline"
 debian_pkg_name="${package_name}"
-package_version="2023.07.20230725.4"
+package_version="2023.07.20230725.5"
 package_source=""
 src_dir=""
 
@@ -19,7 +19,6 @@ debian_patch=""
 
 clear_changelog="enable"
 
-bullseye_version="~bullseye+20230725"
 bookworm_version="~bookworm+20230725"
 trixie_version="~trixie+20230725"
 jammy_version="~jammy+20230725"
