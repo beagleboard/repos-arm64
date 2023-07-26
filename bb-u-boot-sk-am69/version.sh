70363ddfd8fa57b062d1dc67683887cb850ae22e
Robert Nelson <robertcnelson@gmail.com>
2023-07-26 16:51:52 -0500

    bb-u-boot-sk-am69 (2023.04.20230101.0)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

---
+++ bb-u-boot-sk-am69/version.sh
@@ -0,0 +1,22 @@
+#!/bin/bash -e
+
+#https://git.beagleboard.org/beagleboard/u-boot/
+
+package_name="bb-u-boot-sk-am69"
+debian_pkg_name="${package_name}"
+package_version="2023.04.20230101.0"
+package_source=""
+src_dir=""
+
+git_repo=""
+git_sha=""
+reprepro_dir="b/${package_name}"
+dl_path="pool/main/${reprepro_dir}/"
+
+debian_version="${package_version}-0"
+debian_untar=""
+debian_patch=""
+
+clear_changelog="enable"
+
+bookworm_version="~bookworm+20230726"
