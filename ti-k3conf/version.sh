fd737c2ee1b9bcf38db2c86931bb039e054d449a
Robert Nelson <robertcnelson@gmail.com>
2023-07-28 09:52:47 -0500

    ti-k3conf (0.2.0-git20230719.1)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- ti-k3conf/version.sh
+++ ti-k3conf/version.sh
@@ -4,7 +4,7 @@
 
 package_name="ti-k3conf"
 debian_pkg_name="${package_name}"
-package_version="0.2.0-git20230719.0"
+package_version="0.2.0-git20230719.1"
 package_source="${package_name}_${package_version}.orig.tar.xz"
 src_dir="${package_name}_${package_version}"
 
