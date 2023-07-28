0aa98b1d30f4ccb7a8c6902d52a46790f9b3024e
Robert Nelson <robertcnelson@gmail.com>
2023-07-28 17:18:08 -0500

    bb-u-boot-beagleplay-ti-2023.04
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

---
+++ bb-u-boot-beagleplay-ti-2023.04/suite/bookworm/debian/install-microsd.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if ! id | grep -q root; then
+	echo "must be run as root"
+	exit
+fi
+
+wdir="/opt/u-boot/bb-u-boot-beagleplay-ti-2023.04"
+
+if [ -d /boot/firmware/ ] ; then
+	cp -v ${wdir}/tiboot3.bin /boot/firmware/
+	cp -v ${wdir}/tispl.bin /boot/firmware/
+	cp -v ${wdir}/u-boot.img /boot/firmware/
+fi
