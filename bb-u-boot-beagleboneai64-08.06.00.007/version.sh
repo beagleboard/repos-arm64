0649d52366cb234aa67aff7b6a4b0ae1cb24958e
Robert Nelson <robertcnelson@gmail.com>
2023-08-08 18:58:20 -0500

    bb-u-boot-beagleboneai64-08.06.00.007 (2021.10.20230216.7)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

---
+++ bb-u-boot-beagleboneai64-08.06.00.007/version.sh
@@ -0,0 +1,22 @@
+#!/bin/bash -e
+
+#https://git.beagleboard.org/beagleboard/u-boot/-/commits/v2021.01-ti-08.05.00.001
+
+package_name="bb-u-boot-beagleboneai64-08.06.00.007"
+debian_pkg_name="${package_name}"
+package_version="2021.10.20230216.7"
+package_source=""
+src_dir=""
+
+git_repo=""
+git_sha=""
+reprepro_dir="b/${package_name}"
+dl_path="pool/main/${reprepro_dir}/"
+
+debian_version="${package_version}-0"
+debian_untar=""
+debian_patch=""
+
+clear_changelog="enable"
+
+bullseye_version="~bullseye+20230808"
