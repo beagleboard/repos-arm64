03ba0273a26faf31120104928dda512f510268fb
Robert Nelson <robertcnelson@gmail.com>
2023-08-08 22:49:36 -0500

    bb-k3-image-gen-beagleboneai64 (1.20211206.4)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

---
+++ bb-k3-image-gen-beagleboneai64/version.sh
@@ -0,0 +1,25 @@
+#!/bin/bash -e
+
+#https://github.com/beagleboard/k3-image-gen
+
+package_name="bb-k3-image-gen-beagleboneai64"
+debian_pkg_name="${package_name}"
+package_version="1.20211206.4"
+package_source=""
+src_dir=""
+
+git_repo=""
+git_sha=""
+reprepro_dir="b/${package_name}"
+dl_path="pool/main/${reprepro_dir}/"
+
+debian_version="${package_version}-0"
+debian_untar=""
+debian_patch=""
+
+clear_changelog="enable"
+
+bullseye_version="~bullseye+20230808"
+bookworm_version="~bookworm+20230808"
+trixie_version="~trixie+20230808"
+jammy_version="~jammy+20230808"
