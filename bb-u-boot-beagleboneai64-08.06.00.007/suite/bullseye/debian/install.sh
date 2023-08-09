0bbb9a78f1f93844a05407e743915f94f6d121fc
Robert Nelson <robertcnelson@gmail.com>
2023-08-08 23:03:51 -0500

    bb-u-boot-beagleboneai64-08.06.00.007 (2021.10.20230216.16)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- bb-u-boot-beagleboneai64-08.06.00.007/suite/bullseye/debian/install.sh
+++ bb-u-boot-beagleboneai64-08.06.00.007/suite/bullseye/debian/install.sh
@@ -8,7 +8,7 @@ fi
 wdir="/opt/u-boot/bb-u-boot-beagleboneai64"
 
 if [ -d /boot/firmware/ ] ; then
-	cp -v /opt/u-boot/bb-k3-image-gen-j721e-evm/sysfw.itb /boot/firmware/
+	cp -v /opt/u-boot/bb-k3-image-gen-beagleboneai64/sysfw.itb /boot/firmware/
 	cp -v ${wdir}/tiboot3.bin /boot/firmware/
 	cp -v ${wdir}/tispl.bin /boot/firmware/
 	cp -v ${wdir}/u-boot.img /boot/firmware/
