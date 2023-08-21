99d6c310427f87a5d16eecc3f96240af54398635
Robert Nelson <robertcnelson@gmail.com>
2023-08-21 10:03:25 -0500

    bb-u-boot-beagleboneai64-ti-2023.04 (2023.04.20230809.10)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- bb-u-boot-beagleboneai64-ti-2023.04/version.sh
+++ bb-u-boot-beagleboneai64-ti-2023.04/version.sh
@@ -5,7 +5,7 @@
 
 package_name="bb-u-boot-beagleboneai64-ti-2023.04"
 debian_pkg_name="${package_name}"
-package_version="2023.04.20230809.9"
+package_version="2023.04.20230809.10"
 package_source=""
 src_dir=""
 
@@ -20,4 +20,4 @@ debian_patch=""
 
 clear_changelog="enable"
 
-bookworm_version="~bookworm+20230820"
+bookworm_version="~bookworm+20230821"
