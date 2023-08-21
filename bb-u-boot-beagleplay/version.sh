36c3144648d2260c4905553abdf2cd971c19113d
Robert Nelson <robertcnelson@gmail.com>
2023-08-21 14:43:48 -0500

    bb-u-boot-beagleplay (2021.01.20230213.18)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- bb-u-boot-beagleplay/version.sh
+++ bb-u-boot-beagleplay/version.sh
@@ -4,7 +4,7 @@
 
 package_name="bb-u-boot-beagleplay"
 debian_pkg_name="${package_name}"
-package_version="2021.01.20230213.17"
+package_version="2021.01.20230213.18"
 package_source=""
 src_dir=""
 
@@ -19,7 +19,7 @@ debian_patch=""
 
 clear_changelog="enable"
 
-bullseye_version="~bullseye+20230818"
-bookworm_version="~bookworm+20230818"
-trixie_version="~trixie+20230818"
-jammy_version="~jammy+20230818"
+bullseye_version="~bullseye+20230821"
+bookworm_version="~bookworm+20230821"
+trixie_version="~trixie+20230821"
+jammy_version="~jammy+20230821"
