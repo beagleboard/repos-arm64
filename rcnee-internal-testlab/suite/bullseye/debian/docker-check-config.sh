2db63673a6fe512685b24e8e9f30ddf2901b1bc3
Robert Nelson <robertcnelson@gmail.com>
2023-09-06 19:10:11 -0500

    rcnee-internal-testlab (1.20230906.0)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- rcnee-internal-testlab/suite/bullseye/debian/docker-check-config.sh
+++ rcnee-internal-testlab/suite/bullseye/debian/docker-check-config.sh
@@ -320,7 +320,7 @@ check_flags \
 	CGROUP_PERF \
 	CGROUP_HUGETLB \
 	NET_CLS_CGROUP $netprio \
-	CFS_BANDWIDTH FAIR_GROUP_SCHED RT_GROUP_SCHED \
+	CFS_BANDWIDTH FAIR_GROUP_SCHED \
 	IP_NF_TARGET_REDIRECT \
 	IP_VS \
 	IP_VS_NFCT \
@@ -351,7 +351,7 @@ echo "  - \"$(wrap_color 'overlay' blue)\":"
 check_flags VXLAN BRIDGE_VLAN_FILTERING | sed 's/^/    /'
 echo '      Optional (for encrypted networks):'
 check_flags CRYPTO CRYPTO_AEAD CRYPTO_GCM CRYPTO_SEQIV CRYPTO_GHASH \
-	XFRM XFRM_USER XFRM_ALGO INET_ESP | sed 's/^/      /'
+	XFRM XFRM_USER XFRM_ALGO INET_ESP NETFILTER_XT_MATCH_BPF | sed 's/^/      /'
 if [ "$kernelMajor" -lt 5 ] || [ "$kernelMajor" -eq 5 -a "$kernelMinor" -le 3 ]; then
 	check_flags INET_XFRM_MODE_TRANSPORT | sed 's/^/      /'
 fi
@@ -368,22 +368,9 @@ EXITCODE=0
 STORAGE=1
 
 echo '- Storage Drivers:'
-echo "  - \"$(wrap_color 'aufs' blue)\":"
-check_flags AUFS_FS | sed 's/^/    /'
-if ! is_set AUFS_FS && grep -q aufs /proc/filesystems; then
-	echo "      $(wrap_color '(note that some kernels include AUFS patches but not the AUFS_FS flag)' bold black)"
-fi
-[ "$EXITCODE" = 0 ] && STORAGE=0
-EXITCODE=0
-
 echo "  - \"$(wrap_color 'btrfs' blue)\":"
 check_flags BTRFS_FS | sed 's/^/    /'
 check_flags BTRFS_FS_POSIX_ACL | sed 's/^/    /'
-[ "$EXITCODE" = 0 ] && STORAGE=0
-EXITCODE=0
-
-echo "  - \"$(wrap_color 'devicemapper' blue)\":"
-check_flags BLK_DEV_DM DM_THIN_PROVISIONING | sed 's/^/    /'
 [ "$EXITCODE" = 0 ] && STORAGE=0
 EXITCODE=0
 
