3b0b6d4810ea0c1e20c650a1bf990442a1d08078
Robert Nelson <robertcnelson@gmail.com>
2023-09-10 20:33:55 -0500

    mesa (23.1.7-1.git20230906.0)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- debian-12-git-mesa/version.sh
+++ debian-12-git-mesa/version.sh
@@ -5,12 +5,12 @@ mirror="http://http.debian.net/debian"
 
 package_name="mesa"
 debian_pkg_name="${package_name}"
-package_version="23.1.4-1.git20230721.0"
+package_version="23.1.7-1.git20230906.0"
 package_source="${debian_pkg_name}_${package_version}.orig.tar.xz"
 src_dir="${package_name}_${package_version}"
 
 git_repo="https://gitlab.freedesktop.org/mesa/mesa"
-git_sha="72a48f24cceabd8266e45d3f3df35173b17fdb4d"
+git_sha="5b02bc6f0ee38f5765a0934fecc06288e15ab759"
 reprepro_dir="m/${package_name}"
 dl_path=""
 
@@ -18,4 +18,4 @@ debian_version="${package_version}-0"
 debian_patch=""
 debian_diff=""
 
-bookworm_version="~bookworm+20230721"
+bookworm_version="~bookworm+20230910"
