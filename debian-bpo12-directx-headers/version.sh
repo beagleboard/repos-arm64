9e9ffc31fd399099661a843d05ba8796e7295f98
Robert Nelson <robertcnelson@gmail.com>
2023-09-10 23:59:46 -0500

    add: debian-bpo12-directx-headers
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

---
+++ debian-bpo12-directx-headers/version.sh
@@ -0,0 +1,21 @@
+#!/bin/bash -e
+
+#https://packages.debian.org/source/bookworm/directx-headers
+mirror="https://deb.debian.org/debian"
+
+package_name="directx-headers"
+debian_pkg_name="${package_name}"
+package_version="1.610.2"
+
+git_repo=""
+git_sha=""
+reprepro_dir="d/${package_name}"
+dl_path="pool/main/${reprepro_dir}/"
+
+debian_version="${package_version}-2"
+debian_untar="${package_name}_${debian_version}.debian.tar.xz"
+debian_patch=""
+
+dist="debian"
+suite="bookworm"
+deb_arch="arm64"
