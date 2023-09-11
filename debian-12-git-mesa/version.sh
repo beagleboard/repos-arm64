912ff1d71a118bee62f4395c12068437ba14354e
Robert Nelson <robertcnelson@gmail.com>
2023-09-10 20:58:28 -0500

    mesa: use local mirror
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- debian-12-git-mesa/version.sh
+++ debian-12-git-mesa/version.sh
@@ -9,7 +9,7 @@ package_version="23.1.7-1.git20230906.0"
 package_source="${debian_pkg_name}_${package_version}.orig.tar.xz"
 src_dir="${package_name}_${package_version}"
 
-git_repo="https://gitlab.freedesktop.org/mesa/mesa"
+git_repo="https://gitea.gfnd.rcn-ee.org/mirror/mesa.git"
 git_sha="5b02bc6f0ee38f5765a0934fecc06288e15ab759"
 reprepro_dir="m/${package_name}"
 dl_path=""
