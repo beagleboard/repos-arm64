a457ac48cb1255053d28828d057bc96823ad4e46
Robert Nelson <robertcnelson@gmail.com>
2023-09-25 17:02:28 -0500

    generic-sys-mods (1.20230922.2)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- generic-sys-mods/version.sh
+++ generic-sys-mods/version.sh
@@ -2,7 +2,7 @@
 
 package_name="generic-sys-mods"
 debian_pkg_name="${package_name}"
-package_version="1.20230818.0"
+package_version="1.20230922.2"
 package_source=""
 src_dir=""
 
@@ -17,7 +17,7 @@ debian_patch=""
 
 clear_changelog="enable"
 
-bullseye_version="~bullseye+20230818"
-bookworm_version="~bookworm+20230818"
-trixie_version="~trixie+20230818"
-jammy_version="~jammy+20230818"
+bullseye_version="~bullseye+20230925"
+bookworm_version="~bookworm+20230925"
+trixie_version="~trixie+20230925"
+jammy_version="~jammy+20230925"
