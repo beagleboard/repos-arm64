02a8da39ce4e3453a640fa3b945eb0e358dd302e
Robert Nelson <robertcnelson@gmail.com>
2023-09-26 16:47:34 -0500

    iwd (2.7-2bbbio1)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- debian-12-iwd/version.sh
+++ debian-12-iwd/version.sh
@@ -3,14 +3,14 @@
 #https://tracker.debian.org/pkg/wpa
 #https://packages.debian.org/source/trixie/iwd
 #https://snapshot.debian.org/package/iwd/
-#https://snapshot.debian.org/archive/debian/20230627T212729Z/pool/main/i/iwd/
+#https://snapshot.debian.org/archive/debian/20230809T032342Z/pool/main/i/iwd/iwd_2.7-2.dsc
 
 #mirror="http://http.debian.net/debian"
-mirror="https://snapshot.debian.org/archive/debian/20230627T212729Z"
+mirror="https://snapshot.debian.org/archive/debian/20230809T032342Z"
 
 package_name="iwd"
 debian_pkg_name="${package_name}"
-package_version="2.6"
+package_version="2.7"
 package_source="${debian_pkg_name}_${package_version}.orig.tar.xz"
 src_dir="${package_name}-${package_version}"
 
@@ -19,7 +19,7 @@ git_sha=""
 reprepro_dir="i/${package_name}"
 dl_path="pool/main/${reprepro_dir}/"
 
-debian_version="${package_version}-1"
+debian_version="${package_version}-2"
 debian_untar="${package_name}_${debian_version}.debian.tar.xz"
 debian_patch=""
 local_patch="bbbio1"
