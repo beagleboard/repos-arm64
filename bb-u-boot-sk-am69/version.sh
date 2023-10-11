f5867af6fd62e560113d908b5d01e6bb9f667b78
Robert Nelson <robertcnelson@gmail.com>
2023-10-10 20:46:09 -0500

    bb-u-boot-sk-am69 (2023.04.20231002.0-09.00.00.011)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- bb-u-boot-sk-am69/version.sh
+++ bb-u-boot-sk-am69/version.sh
@@ -5,7 +5,7 @@
 
 package_name="bb-u-boot-sk-am69"
 debian_pkg_name="${package_name}"
-package_version="2023.04.20230809.4"
+package_version="2023.04.20231002.0-09.00.00.011"
 package_source=""
 src_dir=""
 
@@ -20,4 +20,4 @@ debian_patch=""
 
 clear_changelog="enable"
 
-bookworm_version="~bookworm+20230820"
+bookworm_version="~bookworm+20231010"
