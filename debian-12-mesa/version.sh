f7a8cfd08e8af19af1f82aa1742c89ac0626c045
Robert Nelson <robertcnelson@gmail.com>
2023-12-04 19:05:40 -0600

    add stable version
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

---
+++ debian-12-mesa/version.sh
@@ -0,0 +1,22 @@
+#!/bin/bash -e
+
+#https://gitlab.freedesktop.org/mesa/mesa/-/commits/23.2/
+mirror="http://http.debian.net/debian"
+
+package_name="mesa"
+debian_pkg_name="${package_name}"
+package_version="23.2.1"
+package_source="${debian_pkg_name}_${package_version}.orig.tar.gz"
+src_dir="${package_name}-${package_version}"
+
+git_repo=""
+git_sha=""
+reprepro_dir="m/${package_name}"
+dl_path="pool/main/${reprepro_dir}/"
+
+debian_version="${package_version}-1"
+debian_untar=""
+debian_patch="${package_name}_${debian_version}.diff.gz"
+local_patch="bbbio0"
+
+bookworm_version="~bookworm+20231204"
