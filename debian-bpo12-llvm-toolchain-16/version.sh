8e3e81b03dccfac2d6f907476d9a218fa479b8ac
Robert Nelson <robertcnelson@gmail.com>
2023-12-04 19:13:17 -0600

    prep for bpo12-llvm-toolchain-16
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

---
+++ debian-bpo12-llvm-toolchain-16/version.sh
@@ -0,0 +1,22 @@
+#!/bin/bash -e
+
+#https://snapshot.debian.org/package/llvm-toolchain-16/
+#https://packages.debian.org/source/bookworm/llvm-toolchain-16
+mirror="https://deb.debian.org/debian"
+
+package_name="llvm-toolchain-16"
+debian_pkg_name="${package_name}"
+package_version="16.0.6"
+
+git_repo=""
+git_sha=""
+reprepro_dir="l/${package_name}"
+dl_path="pool/main/${reprepro_dir}/"
+
+debian_version="${package_version}-19"
+debian_untar="${package_name}_${debian_version}.debian.tar.xz"
+debian_patch=""
+
+dist="debian"
+suite="bookworm"
+deb_arch="arm64"
