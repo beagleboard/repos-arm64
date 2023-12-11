9e617535d447c8cc9a097194e912325f17d6a247
Robert Nelson <robertcnelson@gmail.com>
2023-12-11 16:28:44 -0600

    bb-u-boot-beagleplay-rc (2023.04.20231002.5-09.00.00.011)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

---
+++ bb-u-boot-beagleplay-debian-11-rc/version.sh
@@ -0,0 +1,23 @@
+#!/bin/bash -e
+
+#https://git.beagleboard.org/beagleboard/u-boot/
+#https://git.ti.com/gitweb?p=ti-u-boot/ti-u-boot.git;a=shortlog;h=refs/heads/ti-u-boot-2023.04
+
+package_name="bb-u-boot-beagleplay-rc"
+debian_pkg_name="${package_name}"
+package_version="2023.04.20231002.5-09.00.00.011"
+package_source=""
+src_dir=""
+
+git_repo=""
+git_sha=""
+reprepro_dir="b/${package_name}"
+dl_path="pool/main/${reprepro_dir}/"
+
+debian_version="${package_version}-0"
+debian_untar=""
+debian_patch=""
+
+clear_changelog="enable"
+
+bullseye_version="~bullseye+20231211"
