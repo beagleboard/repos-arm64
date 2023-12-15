4aea56babd5ad11ecd305d9636f059e915ca2774
Robert Nelson <robertcnelson@gmail.com>
2023-12-14 20:53:18 -0600

    directx-headers (1.611.0-1)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- debian-bpo12-directx-headers/version.sh
+++ debian-bpo12-directx-headers/version.sh
@@ -5,14 +5,14 @@ mirror="https://deb.debian.org/debian"
 
 package_name="directx-headers"
 debian_pkg_name="${package_name}"
-package_version="1.610.2"
+package_version="1.611.0"
 
 git_repo=""
 git_sha=""
 reprepro_dir="d/${package_name}"
 dl_path="pool/main/${reprepro_dir}/"
 
-debian_version="${package_version}-2"
+debian_version="${package_version}-1"
 debian_untar="${package_name}_${debian_version}.debian.tar.xz"
 debian_patch=""
 
