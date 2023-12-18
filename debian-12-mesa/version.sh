ab2c55a31204e03f175269b521a290208b5d490c
Robert Nelson <robertcnelson@gmail.com>
2023-12-18 10:21:54 -0600

    mesa (23.3.1-3bbbio0)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- debian-12-mesa/version.sh
+++ debian-12-mesa/version.sh
@@ -14,9 +14,9 @@ git_sha=""
 reprepro_dir="m/${package_name}"
 dl_path="pool/main/${reprepro_dir}/"
 
-debian_version="${package_version}-1"
+debian_version="${package_version}-3"
 debian_untar=""
 debian_patch="${package_name}_${debian_version}.diff.gz"
 local_patch="bbbio0"
 
-bookworm_version="~bookworm+20231214"
+bookworm_version="~bookworm+20231218"
