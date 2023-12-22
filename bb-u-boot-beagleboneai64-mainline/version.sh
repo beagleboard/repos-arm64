50e59935445da99bc914b598f4870d83711e8c06
Robert Nelson <robertcnelson@gmail.com>
2023-12-22 12:33:40 -0600

    bb-u-boot-beagleboneai64-mainline (2024.01.20231222.0)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

---
+++ bb-u-boot-beagleboneai64-mainline/version.sh
@@ -0,0 +1,26 @@
+#!/bin/bash -e
+
+#https://git.beagleboard.org/beagleboard/u-boot/
+#https://github.com/u-boot/u-boot/commits/next
+
+package_name="bb-u-boot-beagleboneai64-mainline"
+debian_pkg_name="${package_name}"
+package_version="2024.01.20231222.0"
+package_source=""
+src_dir=""
+
+git_repo=""
+git_sha=""
+reprepro_dir="b/${package_name}"
+dl_path="pool/main/${reprepro_dir}/"
+
+debian_version="${package_version}-0"
+debian_untar=""
+debian_patch=""
+
+clear_changelog="enable"
+
+bullseye_version="~bullseye+20231201"
+trixie_version="~trixie+20231201"
+jammy_version="~jammy+20231201"
+bookworm_version="~bookworm+20231222"
