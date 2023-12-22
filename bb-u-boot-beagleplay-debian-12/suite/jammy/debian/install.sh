6d126a9d4d586850b2b1a0ab7e41ac90d1facee6
Robert Nelson <robertcnelson@gmail.com>
2023-12-22 14:35:43 -0600

    bb-u-boot-beagleplay (2023.04.20231206.3-09.01.00.008)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- bb-u-boot-beagleplay-debian-12/suite/jammy/debian/install.sh
+++ bb-u-boot-beagleplay-debian-12/suite/jammy/debian/install.sh
@@ -11,8 +11,4 @@ if [ -d /boot/firmware/ ] ; then
 	cp -v ${wdir}/tiboot3.bin /boot/firmware/
 	cp -v ${wdir}/tispl.bin /boot/firmware/
 	cp -v ${wdir}/u-boot.img /boot/firmware/
-	if [ ! -d /boot/firmware/ti-sysfw/ ] ; then
-		mkdir -p /boot/firmware/ti-sysfw/
-	fi
-	cp -v ${wdir}/ti-fs-stub-firmware-am62x-gp.bin /boot/firmware/ti-sysfw/
 fi
