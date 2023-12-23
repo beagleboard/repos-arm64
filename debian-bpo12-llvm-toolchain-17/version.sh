74657a3e1cc5e179ab3a86ee9b74ab8b60fcbcec
Robert Nelson <robertcnelson@gmail.com>
2023-12-23 13:37:27 -0600

    llvm-toolchain-17_17.0.6-1
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- debian-bpo12-llvm-toolchain-17/version.sh
+++ debian-bpo12-llvm-toolchain-17/version.sh
@@ -3,14 +3,15 @@
 #https://snapshot.debian.org/package/llvm-toolchain-17/
 #https://snapshot.debian.org/archive/debian/20231101T091841Z/pool/main/l/llvm-toolchain-17/llvm-toolchain-17_17.0.4-1.dsc
 #https://snapshot.debian.org/archive/debian/20231114T150217Z/pool/main/l/llvm-toolchain-17/llvm-toolchain-17_17.0.5-1.dsc
+#https://snapshot.debian.org/archive/debian/20231130T032551Z/pool/main/l/llvm-toolchain-17/llvm-toolchain-17_17.0.6-1.dsc
 #https://packages.debian.org/source/trixie/llvm-toolchain-17
 
 mirror="https://deb.debian.org/debian"
-mirror="https://snapshot.debian.org/archive/debian/20231114T150217Z"
+mirror="https://snapshot.debian.org/archive/debian/20231130T032551Z"
 
 package_name="llvm-toolchain-17"
 debian_pkg_name="${package_name}"
-package_version="17.0.5"
+package_version="17.0.6"
 
 git_repo=""
 git_sha=""
