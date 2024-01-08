7ea4d9b8f1c476251d86490789c9839f2450a047
Robert Nelson <robertcnelson@gmail.com>
2024-01-08 17:34:08 -0600

    bb-u-boot-beagleplay-rc (2024.01.20240108.0)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- bb-u-boot-beagleplay-debian-rc/version.sh
+++ bb-u-boot-beagleplay-debian-rc/version.sh
@@ -1,11 +1,11 @@
 #!/bin/bash -e
 
 #https://git.beagleboard.org/beagleboard/u-boot/
-#https://git.ti.com/gitweb?p=ti-u-boot/ti-u-boot.git;a=shortlog;h=refs/heads/ti-u-boot-2023.04
+#https://github.com/u-boot/u-boot/commits/master
 
 package_name="bb-u-boot-beagleplay-rc"
 debian_pkg_name="${package_name}"
-package_version="2024.01.20240103.0"
+package_version="2024.01.20240108.0"
 package_source=""
 src_dir=""
 
@@ -20,5 +20,5 @@ debian_patch=""
 
 clear_changelog="enable"
 
-bullseye_version="~bullseye+20240104"
-bookworm_version="~bookworm+20240104"
+bullseye_version="~bullseye+20240108"
+bookworm_version="~bookworm+20240108"
