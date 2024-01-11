1f44e4fe20729820d440c3322a11c295acbcaa2f
Robert Nelson <robertcnelson@gmail.com>
2024-01-11 11:52:59 -0600

    mesa (23.3.3-1bbbio0)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- debian-12-mesa/version.sh
+++ debian-12-mesa/version.sh
@@ -5,7 +5,7 @@ mirror="http://http.debian.net/debian"
 
 package_name="mesa"
 debian_pkg_name="${package_name}"
-package_version="23.3.2"
+package_version="23.3.3"
 package_source="${debian_pkg_name}_${package_version}.orig.tar.gz"
 src_dir="${package_name}-${package_version}"
 
@@ -14,9 +14,9 @@ git_sha=""
 reprepro_dir="m/${package_name}"
 dl_path="pool/main/${reprepro_dir}/"
 
-debian_version="${package_version}-2"
+debian_version="${package_version}-1"
 debian_untar=""
 debian_patch="${package_name}_${debian_version}.diff.gz"
 local_patch="bbbio0"
 
-bookworm_version="~bookworm+20240105"
+bookworm_version="~bookworm+20240111"
