8da1f46285abd29042543f072b56bce85616e4f9
Robert Nelson <robertcnelson@gmail.com>
2024-01-24 13:08:31 -0600

    pipewire (1.0.1-1~bpo12+1bbbio2)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- debian-12-pipewire/version.sh
+++ debian-12-pipewire/version.sh
@@ -18,6 +18,6 @@ dl_path="pool/main/${reprepro_dir}/"
 debian_version="${package_version}-1~bpo12+1"
 debian_untar="${package_name}_${debian_version}.debian.tar.xz"
 debian_patch=""
-local_patch="bbbio1"
+local_patch="bbbio2"
 
 bookworm_version="~bookworm+20240124"
