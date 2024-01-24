c732a29d505464a857b7aadbca6b88f6a031d72f
Robert Nelson <robertcnelson@gmail.com>
2024-01-24 10:20:01 -0600

    libcamera (0.0.3-6)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- debian-bpo11-libcamera/version.sh
+++ debian-bpo11-libcamera/version.sh
@@ -5,14 +5,14 @@ mirror="https://deb.debian.org/debian"
 
 package_name="libcamera"
 debian_pkg_name="${package_name}"
-package_version="0.0.1"
+package_version="0.0.3"
 
 git_repo=""
 git_sha=""
 reprepro_dir="libc/${package_name}"
 dl_path="pool/main/${reprepro_dir}/"
 
-debian_version="${package_version}-4"
+debian_version="${package_version}-6"
 debian_untar="${package_name}_${debian_version}.debian.tar.xz"
 debian_patch=""
 
