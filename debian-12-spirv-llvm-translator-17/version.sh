9b519cd8d98d882d7371290046419ab0d115a08d
Robert Nelson <robertcnelson@gmail.com>
2024-01-24 09:53:29 -0600

    spirv-llvm-translator-17 (17.0.0-3bbbio3)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- debian-12-spirv-llvm-translator-17/version.sh
+++ debian-12-spirv-llvm-translator-17/version.sh
@@ -17,6 +17,6 @@ dl_path="pool/main/${reprepro_dir}/"
 debian_version="${package_version}-3"
 debian_untar="${package_name}_${debian_version}.debian.tar.xz"
 debian_patch=""
-local_patch="bbbio2"
+local_patch="bbbio3"
 
-bookworm_version="~bookworm+20231223"
+bookworm_version="~bookworm+20240124"
