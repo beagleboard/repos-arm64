3fd0cf41e40a0ec35ccdbf60b78d05cf16bdc9a3
Robert Nelson <robertcnelson@gmail.com>
2024-01-30 15:26:29 -0600

    bb-u-boot-beagleplay-mainline (2024.04.20240129.0)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- bb-u-boot-beagleplay-mainline/version.sh
+++ bb-u-boot-beagleplay-mainline/version.sh
@@ -5,7 +5,7 @@
 
 package_name="bb-u-boot-beagleplay-mainline"
 debian_pkg_name="${package_name}"
-package_version="2024.01.20240108.0"
+package_version="2024.04.20240129.0"
 package_source=""
 src_dir=""
 
@@ -20,8 +20,8 @@ debian_patch=""
 
 clear_changelog="enable"
 
-bullseye_version="~bullseye+20240108"
-bookworm_version="~bookworm+20240108"
-trixie_version="~trixie+20240108"
-jammy_version="~jammy+20240108"
-noble_version="~noble+20240108"
+bullseye_version="~bullseye+20240130"
+bookworm_version="~bookworm+20240130"
+trixie_version="~trixie+20240130"
+jammy_version="~jammy+20240130"
+noble_version="~noble+20240130"
