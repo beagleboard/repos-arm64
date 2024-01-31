32d23c6615e5edfff54099cb2742a1e5430e0244
Robert Nelson <robertcnelson@gmail.com>
2024-01-31 16:03:33 -0600

    cnm-firmware (0.0.0-git20230515.0)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

---
+++ cnm-firmware/version.sh
@@ -0,0 +1,22 @@
+#!/bin/bash -e
+
+#https://git.kernel.org/pub/scm/linux/kernel/git/firmware/linux-firmware.git/log/cnm
+
+package_name="cnm-firmware"
+debian_pkg_name="${package_name}"
+package_version="0.0.0-git20230515.0"
+package_source=""
+src_dir=""
+
+git_repo=""
+git_sha=""
+reprepro_dir="c/${package_name}"
+dl_path=""
+
+debian_version="${package_version}-0"
+debian_patch=""
+debian_diff=""
+
+clear_changelog="enable"
+
+bookworm_version="~bookworm+20240131"
