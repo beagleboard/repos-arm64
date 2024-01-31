34197bbb632fccf4a2dc6bf80c307f2649175c92
Robert Nelson <robertcnelson@gmail.com>
2024-01-31 13:08:36 -0600

    bb-u-boot-j722s (2023.04.20240131.3)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- bb-u-boot-j722s/version.sh
+++ bb-u-boot-j722s/version.sh
@@ -5,7 +5,7 @@
 
 package_name="bb-u-boot-j722s"
 debian_pkg_name="${package_name}"
-package_version="2023.04.2024013.2-09.02.00.002"
+package_version="2023.04.20240131.3-09.02.00.002"
 package_source=""
 src_dir=""
 
