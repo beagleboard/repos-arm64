3e454d82fbc059d74fbfbf8b4766ee5327b5f005
Robert Nelson <robertcnelson@gmail.com>
2024-02-02 10:34:09 -0600

    split: bb-u-boot-beagleboneai64-debian-11 and bb-u-boot-beagleboneai64-debian-12
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

---
+++ bb-u-boot-beagleboneai64-debian-12/version.sh
@@ -0,0 +1,25 @@
+#!/bin/bash -e
+
+#https://git.beagleboard.org/beagleboard/u-boot/-/commits/v2021.01-ti-08.05.00.001
+
+package_name="bb-u-boot-beagleboneai64"
+debian_pkg_name="${package_name}"
+package_version="2021.10.20230216.15"
+package_source=""
+src_dir=""
+
+git_repo=""
+git_sha=""
+reprepro_dir="b/${package_name}"
+dl_path="pool/main/${reprepro_dir}/"
+
+debian_version="${package_version}-0"
+debian_untar=""
+debian_patch=""
+
+clear_changelog="enable"
+
+bullseye_version="~bullseye+20231228"
+bookworm_version="~bookworm+20231228"
+trixie_version="~trixie+20231228"
+jammy_version="~jammy+20231228"
