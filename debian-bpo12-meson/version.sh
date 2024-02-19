8f35cc7f45dfc39d8066740f286b120841a10895
Robert Nelson <robertcnelson@gmail.com>
2024-02-19 10:10:43 -0600

    add: meson (1.2.3-1)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

---
+++ debian-bpo12-meson/version.sh
@@ -0,0 +1,22 @@
+#!/bin/bash -e
+
+#https://packages.debian.org/source/trixie/meson
+
+mirror="https://deb.debian.org/debian"
+
+package_name="meson"
+debian_pkg_name="${package_name}"
+package_version="1.2.3"
+
+git_repo=""
+git_sha=""
+reprepro_dir="m/${package_name}"
+dl_path="pool/main/${reprepro_dir}/"
+
+debian_version="${package_version}-1"
+debian_untar="${package_name}_${debian_version}.debian.tar.xz"
+debian_patch=""
+
+dist="debian"
+suite="bookworm"
+deb_arch="arm64"
