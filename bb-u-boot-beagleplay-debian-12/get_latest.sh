21639a692f02d9b84f1c43793f7da001437a082f
Robert Nelson <robertcnelson@gmail.com>
2024-03-08 13:30:28 -0600

    bb-u-boot-beagleplay (2024.04.20240226.1)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

---
+++ bb-u-boot-beagleplay-debian-12/get_latest.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+dl_src () {
+	if [ -d ./src/ ] ; then
+		rm -rf ./src/
+	fi
+
+	git clone https://openbeagle.org/beagleboard/usr-customizations.git ./src/
+}
+
+
+copy_all () {
+	cp -v ./src/${wfile} ./suite/bookworm/debian/
+	cp -v ./src/${wfile} ./suite/trixie/debian/
+	cp -v ./src/${wfile} ./suite/jammy/debian/
+	cp -v ./src/${wfile} ./suite/noble/debian/
+}
+
+dl_src
+
+wfile="arm64/etc/beagle-flasher/beagleplay-emmc-to-microsd" ; copy_all
+wfile="arm64/etc/beagle-flasher/beagleplay-emmc-to-microsd-w-swap" ; copy_all
+wfile="arm64/etc/beagle-flasher/beagleplay-microsd-to-emmc" ; copy_all
+wfile="arm64/etc/beagle-flasher/beagleplay-microsd-to-emmc-w-swap" ; copy_all
