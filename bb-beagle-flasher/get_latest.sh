ec8d4ac0bad54d72c5e79c1d365c7f781165d738
Robert Nelson <robertcnelson@gmail.com>
2024-03-08 11:37:43 -0600

    bb-beagle-flasher (1.20240308.0)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- bb-beagle-flasher/get_latest.sh
+++ bb-beagle-flasher/get_latest.sh
@@ -8,6 +8,11 @@ dl_src () {
 	git clone https://git.beagleboard.org/beagleboard/usr-customizations.git ./src/
 }
 
+copy_some () {
+	cp -v ./src/${wfile} ./suite/bookworm/debian/
+	cp -v ./src/${wfile} ./suite/trixie/debian/
+}
+
 copy_all () {
 	cp -v ./src/${wfile} ./suite/bullseye/debian/
 	cp -v ./src/${wfile} ./suite/bookworm/debian/
@@ -17,11 +22,11 @@ copy_all () {
 
 dl_src
 
-wfile="arm64/usr/sbin/beagle-flasher" ; copy_all
-wfile="arm64/usr/sbin/beagle-flasher-boot-emmc-rootfs-nvme" ; copy_all
-wfile="arm64/usr/sbin/beagle-flasher-boot-mmc-rootfs-sda" ; copy_all
-wfile="arm64/usr/sbin/enable-beagle-flasher" ; copy_all
-wfile="arm64/usr/sbin/init-beagle-flasher" ; copy_all
+wfile="arm64/usr/sbin/beagle-flasher" ; copy_some
+wfile="arm64/usr/sbin/beagle-flasher-boot-emmc-rootfs-nvme" ; copy_some
+wfile="arm64/usr/sbin/beagle-flasher-boot-mmc-rootfs-sda" ; copy_some
+wfile="arm64/usr/sbin/enable-beagle-flasher" ; copy_some
+wfile="arm64/usr/sbin/init-beagle-flasher" ; copy_some
 wfile="arm64/etc/beagle-flasher/bbai64-emmc-to-microsd" ; copy_all
 wfile="arm64/etc/beagle-flasher/bbai64-emmc-to-nvme" ; copy_all
 wfile="arm64/etc/beagle-flasher/bbai64-microsd-to-emmc" ; copy_all
--- bb-beagle-flasher/version.sh
+++ bb-beagle-flasher/version.sh
@@ -2,7 +2,7 @@
 
 package_name="bb-beagle-flasher"
 debian_pkg_name="${package_name}"
-package_version="1.20231207.0"
+package_version="1.20240308.0"
 package_source=""
 src_dir=""
 
@@ -17,8 +17,8 @@ debian_patch=""
 
 clear_changelog="enable"
 
-bullseye_version="~bullseye+20231207"
-bookworm_version="~bookworm+20231207"
-trixie_version="~trixie+20231207"
-jammy_version="~jammy+20231207"
-noble_version="~noble+20231207"
+bullseye_version="~bullseye+20240308"
+bookworm_version="~bookworm+20240308"
+trixie_version="~trixie+20240308"
+jammy_version="~jammy+20240308"
+noble_version="~noble+20240308"
