d87a0c0c0f4ce9c19fdc7e2df05c985b4c34da24
Robert Nelson <robertcnelson@gmail.com>
2024-03-13 12:21:56 -0500

    bb-wl18xx-firmware (1.20240313.0)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- bb-wl18xx-firmware/get_latest.sh
+++ bb-wl18xx-firmware/get_latest.sh
@@ -5,20 +5,21 @@ dl_src () {
 		rm -rf ./src/
 	fi
 
-	git clone https://git.beagleboard.org/beagleboard/usr-customizations.git ./src/
+	git clone https://openbeagle.org/beagleboard/usr-customizations.git ./src/
 }
 
 copy_all () {
 	cp -v ./src/${wfile} ./suite/bullseye/debian/
+	cp -v ./src/${wfile} ./suite/bookworm/debian/
 }
 
 copy_some () {
-	cp -v ./src/${wfile} ./suite/bullseye/debian/
+	cp -v ./src/${wfile} ./suite/bookworm/debian/
 }
 
 dl_src
 
-wfile="arm32/usr/sbin/bb-wl18xx-select-fw" ; copy_all
+wfile="arm32/usr/sbin/bb-wl18xx-select-fw" ; copy_some
 wfile="arm32/usr/sbin/bb-wl18xx-bluetooth-hciattach" ; copy_all
 
 #wfile="TIInit_11.8.32.bts" ; wdir="/debian/" ; copy_all
