971162e3a93f844364bb6810b6cf5f6d25bdcd2b
Robert Nelson <robertcnelson@gmail.com>
2024-04-01 22:35:10 -0500

    ell (0.64-1bbbio1)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- debian-12-libell/version.sh
+++ debian-12-libell/version.sh
@@ -19,7 +19,7 @@ dl_path="pool/main/${reprepro_dir}/"
 debian_version="${package_version}-1"
 debian_untar="${package_name}_${debian_version}.debian.tar.xz"
 debian_patch=""
-local_patch="bbbio0"
+local_patch="bbbio1"
 
 lunar_version="~lunar+20230919"
 bullseye_version="~bullseye+20240401"
