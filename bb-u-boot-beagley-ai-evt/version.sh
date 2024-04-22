24f8492a38418c9de792e80c6cb44426fe3de718
Robert Nelson <robertcnelson@gmail.com>
2024-04-22 10:37:44 -0500

    bb-u-boot-beagley-ai-evt (2023.04.20240319.2-09.02.00.009)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

---
+++ bb-u-boot-beagley-ai-evt/version.sh
@@ -0,0 +1,24 @@
+#!/bin/bash -e
+
+#https://openbeagle.org/beagleboard/u-boot/
+#https://github.com/u-boot/u-boot/commits/master
+
+package_name="bb-u-boot-beagley-ai-evt"
+debian_pkg_name="${package_name}"
+package_version="2023.04.20240319.2-09.02.00.009"
+package_source=""
+src_dir=""
+
+git_repo=""
+git_sha=""
+reprepro_dir="b/${package_name}"
+dl_path="pool/main/${reprepro_dir}/"
+
+debian_version="${package_version}-0"
+debian_untar=""
+debian_patch=""
+
+clear_changelog="enable"
+
+bullseye_version="~bullseye+20240422"
+bookworm_version="~bookworm+20240422"
