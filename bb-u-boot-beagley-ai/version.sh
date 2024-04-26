0cb468ee498447c9f1a062b47b3cf987a4cbc790
Robert Nelson <robertcnelson@gmail.com>
2024-04-26 14:33:10 -0500

    bb-u-boot-beagley-ai (2023.04.20240319.3)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- bb-u-boot-beagley-ai/version.sh
+++ bb-u-boot-beagley-ai/version.sh
@@ -5,7 +5,7 @@
 
 package_name="bb-u-boot-beagley-ai"
 debian_pkg_name="${package_name}"
-package_version="2023.04.20240319.2-09.02.00.009"
+package_version="2023.04.20240319.3-09.02.00.009"
 package_source=""
 src_dir=""
 
@@ -20,5 +20,5 @@ debian_patch=""
 
 clear_changelog="enable"
 
-bullseye_version="~bullseye+20240422"
-bookworm_version="~bookworm+20240422"
+bullseye_version="~bullseye+20240426"
+bookworm_version="~bookworm+20240426"
