f755da3f9917fed5aae5c00dd8b0a6777c79a809
Robert Nelson <robertcnelson@gmail.com>
2024-05-07 13:51:11 -0500

    nemo-python (6.0.0+ds-1bbbio0)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

---
+++ debian-12-nemo-python/version.sh
@@ -0,0 +1,23 @@
+#!/bin/bash -e
+
+#https://packages.debian.org/source/trixie/nemo-fileroller
+
+mirror="http://http.debian.net/debian"
+
+package_name="nemo-python"
+debian_pkg_name="${package_name}"
+package_version="6.0.0+ds"
+package_source="${debian_pkg_name}_${package_version}.orig.tar.xz"
+src_dir="${package_name}"
+
+git_repo=""
+git_sha=""
+reprepro_dir="n/${package_name}"
+dl_path="pool/main/${reprepro_dir}/"
+
+debian_version="${package_version}-1"
+debian_untar="${package_name}_${debian_version}.debian.tar.xz"
+debian_patch=""
+local_patch="bbbio0"
+
+bookworm_version="~bookworm+20240507"
