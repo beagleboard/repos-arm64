566eab04fead16bed7f71a35c98833336a120d5f
Robert Nelson <robertcnelson@gmail.com>
2024-05-13 14:36:39 -0500

    mesa: fix these
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- debian-12-mesa/version.sh
+++ debian-12-mesa/version.sh
@@ -6,7 +6,7 @@ mirror="http://http.debian.net/debian"
 package_name="mesa"
 debian_pkg_name="${package_name}"
 package_version="24.1.0~rc3"
-package_source="${debian_pkg_name}_${package_version}.orig.tar.gz"
+package_source="${debian_pkg_name}_${package_version}.orig.tar.xz"
 #src_dir="${package_name}-${package_version}"
 src_dir="mesa-24.1.0-rc3"
 
@@ -17,7 +17,7 @@ dl_path="pool/main/${reprepro_dir}/"
 
 debian_version="${package_version}-1"
 debian_untar=""
-debian_patch="${package_name}_${debian_version}.diff.gz"
+debian_patch="${package_name}_${debian_version}.diff.xz"
 local_patch="bbbio0"
 
 bookworm_version="~bookworm+20240513"
