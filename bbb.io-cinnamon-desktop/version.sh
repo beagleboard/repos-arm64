33dde204038bf22fba989d4b6bf0d56308fe4fa9
Robert Nelson <robertcnelson@gmail.com>
2024-05-16 09:54:56 -0500

    bbb.io-cinnamon-desktop (1.20240516.0)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

---
+++ bbb.io-cinnamon-desktop/version.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -e
+
+package_name="bbb.io-cinnamon-desktop"
+debian_pkg_name="${package_name}"
+package_version="1.20240516.0"
+package_source=""
+src_dir=""
+
+git_repo=""
+git_sha=""
+reprepro_dir="b/${package_name}"
+dl_path=""
+
+debian_version="${package_version}-0"
+debian_untar=""
+debian_patch=""
+
+clear_changelog="enable"
+
+bookworm_version="~bookworm+20240516"
