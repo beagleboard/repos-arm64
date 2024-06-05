e116af4a4b7582486c838f0e7f296726b14763ad
Robert Nelson <robertcnelson@gmail.com>
2024-06-05 14:45:32 -0500

    openocd (0.12.0-1bbbio3)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- debian-12-openocd/version.sh
+++ debian-12-openocd/version.sh
@@ -1,24 +1,22 @@
 #!/bin/bash -e
 
 #https://packages.debian.org/source/sid/openocd
-
 mirror="http://http.debian.net/debian"
 
 package_name="openocd"
 debian_pkg_name="${package_name}"
-package_version="0.12.0"
-package_source="${debian_pkg_name}_${package_version}.orig.tar.bz2"
-src_dir="${package_name}-${package_version}"
+package_version="0.12.0-1bbbio3"
+package_source="${debian_pkg_name}_${package_version}.orig.tar.xz"
+src_dir="${package_name}_${package_version}"
 
-git_repo=""
-git_sha=""
+git_repo="https://repo.or.cz/openocd.git"
+git_sha="f76c8de910e1e12f4b180956d0189c9483e949a5"
 reprepro_dir="o/${package_name}"
-dl_path="pool/main/${reprepro_dir}/"
+dl_path=""
 
-debian_version="${package_version}-1"
-debian_untar="${package_name}_${debian_version}.debian.tar.xz"
+debian_version="${package_version}"
 debian_patch=""
-local_patch="bbbio2"
+debian_diff=""
 
 bullseye_version="~bullseye+20240605"
 bookworm_version="~bookworm+20240605"
