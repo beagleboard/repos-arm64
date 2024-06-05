c692cd7f6788441e83361f3e206dc5ae5f1f1b81
Robert Nelson <robertcnelson@gmail.com>
2024-06-05 16:42:47 -0500

    umtp-responder (1.6.5-0bbbio0)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- debian-12-umtp-responder/version.sh
+++ debian-12-umtp-responder/version.sh
@@ -5,12 +5,12 @@ mirror="http://http.debian.net/debian"
 
 package_name="umtp-responder"
 debian_pkg_name="${package_name}"
-package_version="1.6.4-0bbbio0"
+package_version="1.6.5-0bbbio0"
 package_source="${debian_pkg_name}_${package_version}.orig.tar.xz"
 src_dir="${package_name}_${package_version}"
 
 git_repo="https://github.com/viveris/uMTP-Responder.git"
-git_sha="c3eb9796cda1b56c897e4c0f4430aa3fa6d49608"
+git_sha="299f25ea4def90dbbf3faf7f4a84f897028607c1"
 reprepro_dir="u/${package_name}"
 dl_path=""
 
