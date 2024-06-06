2bf5ef74aae9cf3d97468815fa4e98db72cc4a65
Robert Nelson <robertcnelson@gmail.com>
2024-06-06 10:45:38 -0500

    ti-zephyr-firmware (0.0.0-0.git20240606.0)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

---
+++ ti-zephyr-firmware/version.sh
@@ -0,0 +1,26 @@
+#!/bin/bash -e
+
+#https://github.com/glneo/zephyr-firmware
+
+package_name="ti-zephyr-firmware"
+debian_pkg_name="${package_name}"
+package_version="0.0.0-0.git20240606.0"
+package_source=""
+src_dir=""
+
+git_repo=""
+git_sha=""
+reprepro_dir="t/${package_name}"
+dl_path=""
+
+debian_version="${package_version}-0"
+debian_patch=""
+debian_diff=""
+
+clear_changelog="enable"
+
+bullseye_version="~bullseye+20240606"
+bookworm_version="~bookworm+20240606"
+trixie_version="~trixie+20240606"
+jammy_version="~jammy+20240606"
+noble_version="~noble+20240606"
