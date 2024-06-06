688cea9a291b0afa1a605d9b3b2376c6e9cefe4a
Robert Nelson <robertcnelson@gmail.com>
2024-06-06 17:07:47 -0500

    iwd (2.18-1bbbio0)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- debian-12-libell/version.sh
+++ debian-12-libell/version.sh
@@ -21,7 +21,6 @@ debian_untar="${package_name}_${debian_version}.debian.tar.xz"
 debian_patch=""
 local_patch="bbbio0"
 
-lunar_version="~lunar+20230919"
 bullseye_version="~bullseye+20240606"
 bookworm_version="~bookworm+20240606"
 jammy_version="~jammy+20240606"
