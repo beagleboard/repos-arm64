c3f772a1adbd6aa22c749be1eac6eaf298975ca7
Robert Nelson <robertcnelson@gmail.com>
2024-06-10 10:11:27 -0500

    ti-psdk-firmware-09.02.00.05 (9.2.0.5-0bbbio4)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- ti-psdk-firmware-09.02.00.05/suite/bullseye/debian/j722s/install-j722s.sh
+++ ti-psdk-firmware-09.02.00.05/suite/bullseye/debian/j722s/install-j722s.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
 if ! id | grep -q root; then
-	echo "beagle-pin-mux must be run as root:"
-	echo "sudo beagle-pin-mux"
+	echo "./install-j722s.sh must be run as root:"
+	echo "sudo ./install-j722s.sh"
 	exit
 fi
 
+cd /opt/ti-psdk-firmware-09.02.00.05/j722s/vision_apps_eaik/
 
 cp -v ./vx_app_rtos_linux_c7x_1.out /usr/lib/firmware/j722s-c71_0-fw
 cp -v ./vx_app_rtos_linux_c7x_2.out /usr/lib/firmware/j722s-c71_1-fw
 cp -v ./vx_app_rtos_linux_mcu2_0.out /usr/lib/firmware/j722s-main-r5f0_0-fw
 
-sudo update-initramfs -uk `uname -r`
-sudo cp /boot/initrd.img-`uname -r` /boot/firmware/initrd.img
+update-initramfs -uk `uname -r`
+cp -v /boot/initrd.img-`uname -r` /boot/firmware/initrd.img
