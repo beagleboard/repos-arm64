98e091ae8be7128dda8d164f98529af92c1f1c46
Robert Nelson <robertcnelson@gmail.com>
2024-06-12 12:48:01 -0500

    generic-sys-mods (1.20240612.1)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- generic-sys-mods/suite/bullseye/debian/iwd_migration.py
+++ generic-sys-mods/suite/bullseye/debian/iwd_migration.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 #Copied from: https://gist.github.com/Mic92/3ca60e6865a9793423a41db0480275d8
 import glob
 import re
@@ -7,20 +9,6 @@ import subprocess
 import curses.ascii
 import binascii
 from typing import Tuple, Optional
-
-def parse_network_manager(filename: str) -> Tuple[Optional[str], Optional[str]]:
-    with open(filename) as f:
-        ssid = None
-        psk = None
-        for line in f:
-            match = re.match("^ssid=(.+)", line)
-            if match:
-                ssid = match.group(1)
-            match2 = re.match("^psk=(.+)", line)
-            if match2:
-                psk = match2.group(1)
-        return (ssid, psk)
-
 
 def main() -> None:
     networks = {}
@@ -34,18 +22,11 @@ def main() -> None:
             match2 = re.match("\s*psk=\"(.+)\"", line)
             if match2:
                 psk = match2.group(1)
-            match3 = re.match("\s*network\s*=\s*", line)
-            if match3:
-                if ssid and psk:
-                    networks[ssid] = psk
-                ssid = None
-                psk = None
+            if ssid and psk:
+                networks[ssid] = psk
+            ssid = None
+            psk = None
 
-    nm_profiles = glob.glob("/etc/NetworkManager/system-connections/*")
-    for f in nm_profiles:
-        ssid, psk = parse_network_manager(f)
-        if ssid and psk:
-            networks[ssid] = psk
     for ssid, psk in networks.items():
         if not re.match("^[0-9 \-_a-zA-z]+$", ssid):
             hexstr = binascii.hexlify(ssid.encode("utf-8")).decode("ascii")
@@ -53,17 +34,11 @@ def main() -> None:
         else:
             filename = f"{ssid}.psk"
         name = os.path.join("/var/lib/iwd", filename)
-        #if os.path.exists(name):
-        #    continue
-        proc = subprocess.run(["wpa_passphrase", ssid, psk], check=True,capture_output=True)
-        out = proc.stdout.decode("utf-8")
-        preshared_key = out.split("\n")[3].replace("psk=", "").strip()
 
         print(f"{ssid} -> {name}")
         with open(name, "w") as f:
           f.write(f"""[Security]
-PreSharedKey={preshared_key}
-Passphrase={psk}
+PreSharedKey={psk}
 """)
 
 if __name__ == '__main__':
