3e49f5a37f79619e0cf08d0d925eac29048656df
Robert Nelson <robertcnelson@gmail.com>
2024-06-14 17:59:06 -0500

    bb-u-boot-beagley-ai (2023.04.20240319.17) and bb-node-red-installer (3.1.10.0)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- bb-node-red-installer/version.sh
+++ bb-node-red-installer/version.sh
@@ -4,7 +4,7 @@
 
 package_name="bb-node-red-installer"
 debian_pkg_name="${package_name}"
-package_version="3.1.9.0"
+package_version="3.1.10.0"
 package_source=""
 src_dir=""
 
@@ -17,11 +17,10 @@ debian_version="${package_version}-0"
 debian_untar=""
 debian_patch=""
 
-#debian_dl_1="https://nodejs.org/dist/latest-v14.x/node-v14.21.3-linux-arm64.tar.xz"
 debian_dl_1="https://nodejs.org/dist/latest-v16.x/node-v16.20.2-linux-arm64.tar.xz"
 
 clear_changelog="enable"
 
-bullseye_version="~bullseye+20240516"
-bookworm_version="~bookworm+20240516"
-trixie_version="~trixie+20240516"
+bullseye_version="~bullseye+20240614"
+bookworm_version="~bookworm+20240614"
+trixie_version="~trixie+20240614"
