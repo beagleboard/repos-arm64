b86cf37ca2aabe7fd25cec3b5d176a85e2172eb2
Robert Nelson <robertcnelson@gmail.com>
2024-06-20 16:52:13 -0500

    bb-u-boot-beagleplay-mainline (2024.07.20240603.3)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- bb-u-boot-beagleplay-mainline/version.sh
+++ bb-u-boot-beagleplay-mainline/version.sh
@@ -5,7 +5,7 @@
 
 package_name="bb-u-boot-beagleplay-mainline"
 debian_pkg_name="${package_name}"
-package_version="2024.07.20240603.2"
+package_version="2024.07.20240603.3"
 package_source=""
 src_dir=""
 
