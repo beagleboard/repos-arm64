b2bca8df4dc832a181e681b22bb7be979ede9ea3
Robert Nelson <robertcnelson@gmail.com>
2024-06-21 12:28:50 -0500

    bb-node-red-installer-next (3.1.10.0)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

---
+++ bb-node-red-installer-next/version.sh
@@ -0,0 +1,24 @@
+#!/bin/bash -e
+
+#https://www.npmjs.com/package/node-red
+
+package_name="bb-node-red-installer-next"
+debian_pkg_name="${package_name}"
+package_version="3.1.10.0"
+package_source=""
+src_dir=""
+
+git_repo=""
+git_sha=""
+reprepro_dir="b/${package_name}"
+dl_path="pool/main/${reprepro_dir}/"
+
+debian_version="${package_version}-0"
+debian_untar=""
+debian_patch=""
+
+debian_dl_1="https://nodejs.org/dist/latest-v16.x/node-v16.20.2-linux-arm64.tar.xz"
+
+clear_changelog="enable"
+
+bookworm_version="~bookworm+20240621"
