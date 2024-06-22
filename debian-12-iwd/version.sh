0707db33d5de6e4925a0dae1bb3a69a08376cfe9
Robert Nelson <robertcnelson@gmail.com>
2024-06-22 14:04:35 -0500

    iwd (2.18-1bbbio3)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- debian-12-iwd/version.sh
+++ debian-12-iwd/version.sh
@@ -18,7 +18,7 @@ dl_path="pool/main/${reprepro_dir}/"
 debian_version="${package_version}-1"
 debian_untar="${package_name}_${debian_version}.debian.tar.xz"
 debian_patch=""
-local_patch="bbbio2"
+local_patch="bbbio3"
 
 bookworm_version="~bookworm+20240622"
 trixie_version="~trixie+20240622"
