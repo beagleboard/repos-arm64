6fdbebfb11239945b7ccf8765b6d5f46e6aa5ddd
Robert Nelson <robertcnelson@gmail.com>
2024-06-25 15:35:45 -0500

    bb-u-boot-beagleboneai64 (2024.07.20240624.0)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- bb-u-boot-beagleboneai64-debian-12/version.sh
+++ bb-u-boot-beagleboneai64-debian-12/version.sh
@@ -5,7 +5,7 @@
 
 package_name="bb-u-boot-beagleboneai64"
 debian_pkg_name="${package_name}"
-package_version="2024.07.20240603.2"
+package_version="2024.07.20240624.0"
 package_source=""
 src_dir=""
 
