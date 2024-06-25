67e366d13ae5c7b4cdf2f18d151ae3517cad1f64
Robert Nelson <robertcnelson@gmail.com>
2024-06-25 12:32:43 -0500

    bb-beagle-version (1.20240625.0)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- bb-beagle-version/version.sh
+++ bb-beagle-version/version.sh
@@ -4,7 +4,7 @@
 
 package_name="bb-beagle-version"
 debian_pkg_name="${package_name}"
-package_version="1.20240622.0"
+package_version="1.20240625.0"
 package_source=""
 src_dir=""
 
@@ -19,8 +19,8 @@ debian_patch=""
 
 clear_changelog="enable"
 
-bullseye_version="~bullseye+20240622"
-bookworm_version="~bookworm+20240622"
-trixie_version="~trixie+20240622"
-jammy_version="~jammy+20240622"
-noble_version="~noble+20240622"
+bullseye_version="~bullseye+20240625"
+bookworm_version="~bookworm+20240625"
+trixie_version="~trixie+20240625"
+jammy_version="~jammy+20240625"
+noble_version="~noble+20240625"
