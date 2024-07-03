8c836b3fd50b91dded777143778f3716e1074cf0
Robert Nelson <robertcnelson@gmail.com>
2024-07-03 10:11:20 -0500

    bb-node-red-installer (4.0.2-0bbbio0)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- bb-node-red-installer/version.sh
+++ bb-node-red-installer/version.sh
@@ -4,7 +4,7 @@
 
 package_name="bb-node-red-installer"
 debian_pkg_name="${package_name}"
-package_version="4.0.1"
+package_version="4.0.2"
 package_source=""
 src_dir=""
 
@@ -21,7 +21,7 @@ debian_dl_1="https://nodejs.org/dist/latest-v18.x/node-v18.20.3-linux-arm64.tar.
 
 clear_changelog="enable"
 
-bullseye_version="~bullseye+20240626"
-bookworm_version="~bookworm+20240626"
-trixie_version="~trixie+20240626"
-noble_version="~noble+20240626"
+bullseye_version="~bullseye+20240703"
+bookworm_version="~bookworm+20240703"
+trixie_version="~trixie+20240703"
+noble_version="~noble+20240703"
