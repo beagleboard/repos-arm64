7026a8a257a47ae51d8f3ebf276457a22b20ba46
Robert Nelson <robertcnelson@gmail.com>
2024-07-10 08:29:27 -0500

    libdrm-dev (2.4.121-2)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- debian-bpo12-libdrm/version.sh
+++ debian-bpo12-libdrm/version.sh
@@ -4,14 +4,14 @@ mirror="https://deb.debian.org/debian"
 
 package_name="libdrm"
 debian_pkg_name="${package_name}"
-package_version="2.4.120"
+package_version="2.4.121"
 
 git_repo=""
 git_sha=""
 reprepro_dir="libd/${package_name}"
 dl_path="pool/main/${reprepro_dir}/"
 
-debian_version="${package_version}-1"
+debian_version="${package_version}-2"
 debian_untar="${package_name}_${debian_version}.debian.tar.xz"
 debian_patch=""
 
