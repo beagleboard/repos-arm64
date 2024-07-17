5c97e4baa23d4bc47f1ce5b4c785d338630a5909
Robert Nelson <robertcnelson@gmail.com>
2024-07-17 14:52:32 -0500

    openocd (0.12.0-1bbbio8)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- debian-12-openocd/version.sh
+++ debian-12-openocd/version.sh
@@ -5,12 +5,12 @@ mirror="http://http.debian.net/debian"
 
 package_name="openocd"
 debian_pkg_name="${package_name}"
-package_version="0.12.0-1bbbio7"
+package_version="0.12.0-1bbbio8"
 package_source="${debian_pkg_name}_${package_version}.orig.tar.xz"
 src_dir="${package_name}_${package_version}"
 
 git_repo="https://repo.or.cz/openocd.git"
-git_sha="437dde701c13e707e5fd912ef6403e09052e4d9b"
+git_sha="7f2d3e2925833c952ee73fb178c8fdee637c844e"
 reprepro_dir="o/${package_name}"
 dl_path=""
 
