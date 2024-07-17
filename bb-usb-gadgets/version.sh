a04544f13e40b8ec53358877b69ec4351e2fa7ba
Robert Nelson <robertcnelson@gmail.com>
2024-07-17 13:40:48 -0500

    bb-usb-gadgets (1.20240717.0)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- bb-usb-gadgets/version.sh
+++ bb-usb-gadgets/version.sh
@@ -2,7 +2,7 @@
 
 package_name="bb-usb-gadgets"
 debian_pkg_name="${package_name}"
-package_version="1.20240711.0"
+package_version="1.20240717.0"
 package_source=""
 src_dir=""
 
@@ -17,8 +17,8 @@ debian_patch=""
 
 clear_changelog="enable"
 
-bullseye_version="~bullseye+20240711"
-bookworm_version="~bookworm+20240711"
-trixie_version="~trixie+20240711"
-jammy_version="~jammy+20240711"
-noble_version="~noble+20240711"
+bullseye_version="~bullseye+20240717"
+bookworm_version="~bookworm+20240717"
+trixie_version="~trixie+20240717"
+jammy_version="~jammy+20240717"
+noble_version="~noble+20240717"
