fa9816090f9a8b162fa82d536c5329abc357a933
Robert Nelson <robertcnelson@gmail.com>
2025-01-31 11:57:40 -0600

    bb-u-boot-pocketbeagle2 (2025.01.20250107.5)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- bb-u-boot-pocketbeagle2-debian-12/suite/noble/debian/install.sh
+++ bb-u-boot-pocketbeagle2-debian-12/suite/noble/debian/install.sh
@@ -11,4 +11,10 @@ if [ -d /boot/firmware/ ] ; then
 	cp -v ${wdir}/tiboot3.bin /boot/firmware/
 	cp -v ${wdir}/tispl.bin /boot/firmware/
 	cp -v ${wdir}/u-boot.img /boot/firmware/
+	if [ -f /boot/firmware/extlinux/extlinux.conf ] ; then
+		echo "Patching extlinux.conf"
+		sed -i -s 's:ttyS4:ttyS2:g' /boot/firmware/extlinux/extlinux.conf
+		sed -i -e 's:2800000:2860000:g' /boot/firmware/extlinux/extlinux.conf
+		sed -i -e 's:append root:append console=ttyS2,115200n8 root:g' /boot/firmware/extlinux/extlinux.conf
+	fi
 fi
