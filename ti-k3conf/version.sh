4de496b80f29454e03de32a9b2595739e43d1f1c
Robert Nelson <robertcnelson@gmail.com>
2025-02-03 16:11:37 -0600

    ti-k3conf (0.3.0-0.git20240725.0)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- ti-k3conf/version.sh
+++ ti-k3conf/version.sh
@@ -4,12 +4,12 @@
 
 package_name="ti-k3conf"
 debian_pkg_name="${package_name}"
-package_version="0.3.0-0.git20240306.0"
+package_version="0.3.0-0.git20240725.0"
 package_source="${package_name}_${package_version}.orig.tar.xz"
 src_dir="${package_name}_${package_version}"
 
 git_repo="https://git.ti.com/git/k3conf/k3conf.git"
-git_sha="85a7433202aafafbccea8a7b2491f4df8b1ecd80"
+git_sha="30a1d5b2d08c0da39d4e62b3ebee591a9882fa21"
 reprepro_dir="t/${package_name}"
 dl_path=""
 
@@ -19,7 +19,8 @@ debian_diff=""
 
 clear_changelog="enable"
 
-bullseye_version="~bullseye+20240327"
-bookworm_version="~bookworm+20240327"
-trixie_version="~trixie+20240327"
-jammy_version="~jammy+20240327"
+bullseye_version="~bullseye+20250203"
+bookworm_version="~bookworm+20250203"
+trixie_version="~trixie+20250203"
+jammy_version="~jammy+20250203"
+noble_version="~noble+20250203"
