fbbf822bbaed27e7f11cb986c4cb273e37942342
Robert Nelson <robertcnelson@gmail.com>
2025-02-13 09:53:51 -0600

    directx: 1.614.1
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- debian-bpo12-directx-headers/version.sh
+++ debian-bpo12-directx-headers/version.sh
@@ -1,18 +1,18 @@
 #!/bin/bash -e
 
-#https://packages.debian.org/source/bookworm/directx-headers
+#https://packages.debian.org/source/bookworm-backports/directx-headers
 mirror="https://deb.debian.org/debian"
 
 package_name="directx-headers"
 debian_pkg_name="${package_name}"
-package_version="1.613.0"
+package_version="1.614.1"
 
 git_repo=""
 git_sha=""
 reprepro_dir="d/${package_name}"
 dl_path="pool/main/${reprepro_dir}/"
 
-debian_version="${package_version}-1"
+debian_version="${package_version}-1~bpo12+1"
 debian_untar="${package_name}_${debian_version}.debian.tar.xz"
 debian_patch=""
 
