964fab85e648dfa165168281649fbc437719a547
Robert Nelson <robertcnelson@gmail.com>
2025-02-13 10:01:46 -0600

    wayland-protocols (1.39-1~bpo12+2bbbio0)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- debian-12-wayland-protocols/version.sh
+++ debian-12-wayland-protocols/version.sh
@@ -1,10 +1,11 @@
 #!/bin/bash -e
 
-mirror="http://http.debian.net/debian"
+#mirror="http://http.debian.net/debian"
+mirror="https://snapshot.debian.org/archive/debian/20250106T154800Z"
 
 package_name="wayland-protocols"
 debian_pkg_name="${package_name}"
-package_version="1.34"
+package_version="1.39"
 package_source="${debian_pkg_name}_${package_version}.orig.tar.xz"
 src_dir="${package_name}-${package_version}"
 
@@ -13,9 +14,9 @@ git_sha=""
 reprepro_dir="w/${package_name}"
 dl_path="pool/main/${reprepro_dir}/"
 
-debian_version="${package_version}-1"
+debian_version="${package_version}-1~bpo12+2"
 debian_untar="${package_name}_${debian_version}.debian.tar.xz"
 debian_patch=""
 local_patch="bbbio0"
 
-bookworm_version="~bookworm+20240403"
+bookworm_version="~bookworm+20250213"
