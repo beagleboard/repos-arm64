ae6185f4ac319332437f8beeea4fbccb935658a6
Robert Nelson <robertcnelson@gmail.com>
2025-02-27 16:13:56 -0600

    bb-u-boot-pocketbeagle2 (2025.04.20250224.0)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- bb-u-boot-pocketbeagle2-debian-12/version.sh
+++ bb-u-boot-pocketbeagle2-debian-12/version.sh
@@ -5,7 +5,7 @@
 
 package_name="bb-u-boot-pocketbeagle2"
 debian_pkg_name="${package_name}"
-package_version="2025.04.20250210.2"
+package_version="2025.04.20250224.0"
 package_source=""
 src_dir=""
 
