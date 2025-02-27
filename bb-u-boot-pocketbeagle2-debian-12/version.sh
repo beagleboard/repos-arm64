d3dd4b762b974c10bc47a6523e8759197e1f0155
Robert Nelson <robertcnelson@gmail.com>
2025-02-27 10:38:55 -0600

    bb-u-boot-pocketbeagle2 (2025.04.20250210.2)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- bb-u-boot-pocketbeagle2-debian-12/version.sh
+++ bb-u-boot-pocketbeagle2-debian-12/version.sh
@@ -5,7 +5,7 @@
 
 package_name="bb-u-boot-pocketbeagle2"
 debian_pkg_name="${package_name}"
-package_version="2025.04.20250210.1"
+package_version="2025.04.20250210.2"
 package_source=""
 src_dir=""
 
@@ -20,6 +20,6 @@ debian_patch=""
 
 clear_changelog="enable"
 
-bookworm_version="~bookworm+20250214"
-trixie_version="~trixie+20250214"
-noble_version="~noble+20250214"
+bookworm_version="~bookworm+20250227"
+trixie_version="~trixie+20250227"
+noble_version="~noble+20250227"
