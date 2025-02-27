34b52af17b5382604464a8aebb587d16a5891100
Robert Nelson <robertcnelson@gmail.com>
2025-02-27 15:32:19 -0600

    fixes
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- debian-12-firmware-nonfree/version.sh
+++ debian-12-firmware-nonfree/version.sh
@@ -11,7 +11,7 @@ src_dir="${package_name}-${package_version}"
 git_repo=""
 git_sha=""
 reprepro_dir="f/${package_name}"
-dl_path="pool/non-free/${reprepro_dir}/"
+dl_path="pool/non-free-firmware/${reprepro_dir}/"
 
 debian_version="${package_version}-2~bpo12+1"
 debian_untar="${package_name}_${debian_version}.debian.tar.xz"
