a244fa715d7ceceac62b12379f83665e086f47be
Robert Nelson <robertcnelson@gmail.com>
2025-03-04 11:21:16 -0600

    ti-devmem2 (0.0.0-git20210904.4)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- ti-devmem2/version.sh
+++ ti-devmem2/version.sh
@@ -4,7 +4,7 @@
 
 package_name="ti-devmem2"
 debian_pkg_name="${package_name}"
-package_version="0.0.0-git20210904.3"
+package_version="0.0.0-git20210904.4"
 package_source="${package_name}_${package_version}.orig.tar.xz"
 src_dir="${package_name}_${package_version}"
 
@@ -19,5 +19,7 @@ debian_diff=""
 
 clear_changelog="enable"
 
-bullseye_version="~bullseye+20230405"
-bookworm_version="~bookworm+20230405"
+bullseye_version="~bullseye+20250304"
+bookworm_version="~bookworm+20250304"
+trixie_version="~trixie+20250304"
+noble_version="~noble+20250304"
