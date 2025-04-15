54c6fa19ff82d16a1ca360c13fb879dbfd0036f6
Robert Nelson <robertcnelson@gmail.com>
2025-04-15 15:10:43 -0500

    bb-u-boot-sk-am69 (2025.04.20250407.0)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- bb-u-boot-sk-am69/version.sh
+++ bb-u-boot-sk-am69/version.sh
@@ -5,7 +5,7 @@
 
 package_name="bb-u-boot-sk-am69"
 debian_pkg_name="${package_name}"
-package_version="2025.04.20250324.0"
+package_version="2025.04.20250407.0"
 package_source=""
 src_dir=""
 
@@ -20,4 +20,4 @@ debian_patch=""
 
 clear_changelog="enable"
 
-bookworm_version="~bookworm+20250326"
+bookworm_version="~bookworm+20250415"
