f7ef4d4a8f975be636a61bc05a3592d76fd10a55
Robert Nelson <robertcnelson@gmail.com>
2025-04-16 15:17:39 -0500

    Revert "meson (1.5.1-1~bpo12+1bbbio0)"
    This reverts commit 724894af6897c1aa755caa0987e8cd8a60ed45b3.
    
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- debian-12-meson/version.sh
+++ debian-12-meson/version.sh
@@ -2,12 +2,12 @@
 
 #https://packages.debian.org/source/trixie/meson
 
-mirror="https://deb.debian.org/debian"
-#mirror="https://snapshot.debian.org/archive/debian/20240214T085951Z"
+#mirror="https://deb.debian.org/debian"
+mirror="https://snapshot.debian.org/archive/debian/20240214T085951Z"
 
 package_name="meson"
 debian_pkg_name="${package_name}"
-package_version="1.5.1"
+package_version="1.3.2"
 package_source="${debian_pkg_name}_${package_version}.orig.tar.gz"
 src_dir="${package_name}-${package_version}"
 
@@ -16,9 +16,9 @@ git_sha=""
 reprepro_dir="m/${package_name}"
 dl_path="pool/main/${reprepro_dir}/"
 
-debian_version="${package_version}-1~bpo12+1"
+debian_version="${package_version}-1"
 debian_untar=""
 debian_patch="${package_name}_${debian_version}.debian.tar.xz"
 local_patch="bbbio0"
 
-bookworm_version="~bookworm+20250416"
+bookworm_version="~bookworm+20240330"
