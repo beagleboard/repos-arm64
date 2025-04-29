76ca8139481adf76777ec4537c5932f248d75110
Robert Nelson <robertcnelson@gmail.com>
2025-04-29 18:48:39 -0500

    bb-u-boot-beagley-ai-mainline (2025.07.20250428.0)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

---
+++ bb-u-boot-beagley-ai-mainline/version.sh
@@ -0,0 +1,26 @@
+#!/bin/bash -e
+
+#https://openbeagle.org/beagleboard/u-boot/
+#https://github.com/u-boot/u-boot/commits/master
+
+package_name="bb-u-boot-beagley-ai-mainline"
+debian_pkg_name="${package_name}"
+package_version="2025.07.20250428.0"
+package_source=""
+src_dir=""
+
+git_repo=""
+git_sha=""
+reprepro_dir="b/${package_name}"
+dl_path="pool/main/${reprepro_dir}/"
+
+debian_version="${package_version}-0"
+debian_untar=""
+debian_patch=""
+
+clear_changelog="enable"
+
+bullseye_version="~bullseye+20250429"
+bookworm_version="~bookworm+20250429"
+trixie_version="~trixie+20250429"
+noble_version="~noble+20250429"
