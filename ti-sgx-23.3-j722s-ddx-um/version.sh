84045d91856dbbe4e561355001909c5a3dff7473
Robert Nelson <robertcnelson@gmail.com>
2025-05-06 15:17:23 -0500

    ti-sgx-23.3-j722s-ddx-um (23.3.6512818.0)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- ti-sgx-23.3-j722s-ddx-um/version.sh
+++ ti-sgx-23.3-j722s-ddx-um/version.sh
@@ -16,8 +16,8 @@ dl_path=""
 debian_version="${package_version}-0"
 debian_patch=""
 debian_diff=""
-local_patch="bbbio0"
+local_patch="bbbio1"
 
 clear_changelog="enable"
 
-bookworm_version="~bookworm+20240507"
+bookworm_version="~bookworm+20250506"
