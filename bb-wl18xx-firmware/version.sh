c352caa8df7edd1d643b70f207b9b3675346a535
Robert Nelson <robertcnelson@gmail.com>
2025-05-30 14:04:44 -0500

    bb-wl18xx-firmware (1.20240622.4)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- bb-wl18xx-firmware/version.sh
+++ bb-wl18xx-firmware/version.sh
@@ -2,7 +2,7 @@
 
 package_name="bb-wl18xx-firmware"
 debian_pkg_name="${package_name}"
-package_version="1.20240622.3"
+package_version="1.20240622.4"
 package_source=""
 src_dir=""
 
@@ -17,7 +17,7 @@ debian_patch=""
 
 clear_changelog="enable"
 
-bullseye_version="~bullseye+20250523"
-bookworm_version="~bookworm+20250523"
-trixie_version="~trixie+20250523"
-noble_version="~noble+20250523"
+bullseye_version="~bullseye+20250530"
+bookworm_version="~bookworm+20250530"
+trixie_version="~trixie+20250530"
+noble_version="~noble+20250530"
