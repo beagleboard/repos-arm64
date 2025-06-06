ef28e6dd08c979bde62c017eee9c37e346a804b6
Robert Nelson <robertcnelson@gmail.com>
2025-06-05 19:23:20 -0500

    use local mirror
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- ti-k3conf/version.sh
+++ ti-k3conf/version.sh
@@ -8,7 +8,7 @@ package_version="0.3.0-0.git20250522.0"
 package_source="${package_name}_${package_version}.orig.tar.xz"
 src_dir="${package_name}_${package_version}"
 
-git_repo="https://git.ti.com/git/k3conf/k3conf.git"
+git_repo="http://forgejo.gfnd.rcn-ee.org:3000/TexasInstruments/k3conf.git"
 git_sha="ea9938bf9cafd9c0cb05ef060a713ab4ac72270f"
 reprepro_dir="t/${package_name}"
 dl_path=""
