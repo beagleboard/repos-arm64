3aac2a5a2df385b4435da936fdd333cc05232d6e
Robert Nelson <robertcnelson@gmail.com>
2025-06-12 10:01:54 -0500

    Revert "ti-k3conf (0.3.0-0.git20250522.1)"
    This reverts commit f32ca4b0be6791d045ab3efd42177f9d8a8a1e54.
    
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- ti-k3conf/version.sh
+++ ti-k3conf/version.sh
@@ -4,7 +4,7 @@
 
 package_name="ti-k3conf"
 debian_pkg_name="${package_name}"
-package_version="0.3.0-0.git20250522.1"
+package_version="0.3.0-0.git20250522.0"
 package_source="${package_name}_${package_version}.orig.tar.xz"
 src_dir="${package_name}_${package_version}"
 
@@ -19,7 +19,7 @@ debian_diff=""
 
 clear_changelog="enable"
 
-bullseye_version="~bullseye+20250612"
-bookworm_version="~bookworm+20250612"
-trixie_version="~trixie+20250612"
-noble_version="~noble+20250612"
+bullseye_version="~bullseye+20250605"
+bookworm_version="~bookworm+20250605"
+trixie_version="~trixie+20250605"
+noble_version="~noble+20250605"
