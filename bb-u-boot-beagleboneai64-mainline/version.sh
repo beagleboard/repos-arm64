f07b9c7e7282d21b551e1f752c0fc267ff82c842
Robert Nelson <robertcnelson@gmail.com>
2025-07-08 17:53:30 -0500

    bb-u-boot-beagleboneai64-mainline (2025.07.20250707.0)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- bb-u-boot-beagleboneai64-mainline/version.sh
+++ bb-u-boot-beagleboneai64-mainline/version.sh
@@ -5,7 +5,7 @@
 
 package_name="bb-u-boot-beagleboneai64-mainline"
 debian_pkg_name="${package_name}"
-package_version="2025.07.20250623.0"
+package_version="2025.07.20250707.0"
 package_source=""
 src_dir=""
 
@@ -20,7 +20,7 @@ debian_patch=""
 
 clear_changelog="enable"
 
-bullseye_version="~bullseye+20250625"
-bookworm_version="~bookworm+20250625"
-trixie_version="~trixie+20250625"
-noble_version="~noble+20250625"
+bullseye_version="~bullseye+20250708"
+bookworm_version="~bookworm+20250708"
+trixie_version="~trixie+20250708"
+noble_version="~noble+20250708"
