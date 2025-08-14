4466c2b4c46dde86cc23f03eb3f95cd1a9c7b5ab
Robert Nelson <robertcnelson@gmail.com>
2025-08-14 14:29:48 -0500

    mesa (25.2.0-1bbbio0)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

---
+++ debian-13-mesa/version.sh
@@ -0,0 +1,23 @@
+#!/bin/bash -e
+
+mirror="http://http.debian.net/debian"
+
+package_name="mesa"
+debian_pkg_name="${package_name}"
+package_version="25.2.0"
+package_source="${debian_pkg_name}_${package_version}.orig.tar.xz"
+src_dir="${package_name}-${package_version}"
+#src_dir="mesa-24.1.0-rc3"
+
+git_repo=""
+git_sha=""
+reprepro_dir="m/${package_name}"
+dl_path="pool/main/${reprepro_dir}/"
+
+debian_version="${package_version}-1"
+debian_untar="${package_name}_${debian_version}.debian.tar.xz"
+debian_patch=""
+local_patch="bbbio0"
+
+bookworm_version="~bookworm+20250507"
+trixie_version="~trixie+20250814"
