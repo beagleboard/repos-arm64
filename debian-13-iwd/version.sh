191989150c957ed432d951fec4a0bb24c9032c90
Robert Nelson <robertcnelson@gmail.com>
2025-08-14 12:28:16 -0500

    iwd (3.9-2bbbio0)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

---
+++ debian-13-iwd/version.sh
@@ -0,0 +1,23 @@
+#!/bin/bash -e
+
+#https://packages.debian.org/source/forky/iwd
+
+mirror="http://http.debian.net/debian"
+
+package_name="iwd"
+debian_pkg_name="${package_name}"
+package_version="3.9"
+package_source="${debian_pkg_name}_${package_version}.orig.tar.xz"
+src_dir="${package_name}-${package_version}"
+
+git_repo=""
+git_sha=""
+reprepro_dir="i/${package_name}"
+dl_path="pool/main/${reprepro_dir}/"
+
+debian_version="${package_version}-2"
+debian_untar="${package_name}_${debian_version}.debian.tar.xz"
+debian_patch=""
+local_patch="bbbio0"
+
+forky_version="~forky+20250814"
