f0dcf6284a8046c05265ad3acb75d6e612ab7855
Robert Nelson <robertcnelson@gmail.com>
2025-08-14 21:12:56 -0500

    bbb.io-xfce4-desktop (1.20250814.0)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- bbb.io-xfce4-desktop/version.sh
+++ bbb.io-xfce4-desktop/version.sh
@@ -2,7 +2,7 @@
 
 package_name="bbb.io-xfce4-desktop"
 debian_pkg_name="${package_name}"
-package_version="1.20240627.0"
+package_version="1.20250814.0"
 package_source=""
 src_dir=""
 
@@ -17,6 +17,5 @@ debian_patch=""
 
 clear_changelog="enable"
 
-bullseye_version="~bullseye+20240627"
-bookworm_version="~bookworm+20240627"
-trixie_version="~trixie+20240627"
+bookworm_version="~bookworm+20250814"
+trixie_version="~trixie+20250814"
