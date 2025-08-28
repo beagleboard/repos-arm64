f324656a894d1e1371195dc06332c96eb0c93fe4
Robert Nelson <robertcnelson@gmail.com>
2025-08-28 16:33:32 -0500

    bb-code-server (4.103.2-0bbbio1)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- bb-code-server/version.sh
+++ bb-code-server/version.sh
@@ -16,7 +16,7 @@ git_sha=""
 reprepro_dir="b/${package_name}"
 dl_path="pool/main/${reprepro_dir}/"
 
-debian_version="${package_version}-0bbbio0"
+debian_version="${package_version}-0bbbio1"
 debian_untar=""
 debian_patch=""
 
