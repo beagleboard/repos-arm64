345861d3bfc4750042202bb26df4abd1ae2a6854
Robert Nelson <robertcnelson@gmail.com>
2025-08-28 14:15:34 -0500

    ti-k3conf (0.3.0-0.git20250828.0)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- ti-k3conf/version.sh
+++ ti-k3conf/version.sh
@@ -4,12 +4,12 @@
 
 package_name="ti-k3conf"
 debian_pkg_name="${package_name}"
-package_version="0.3.0-0.git20250807.0"
+package_version="0.3.0-0.git20250828.0"
 package_source="${package_name}_${package_version}.orig.tar.xz"
 src_dir="${package_name}_${package_version}"
 
 git_repo="http://forgejo.gfnd.rcn-ee.org:3000/TexasInstruments/k3conf.git"
-git_sha="b35ec6f6c6e11471c5d06f8c24ac396077f893be"
+git_sha="3de294db4f2cd75404f358768f4be8f8fa3132d7"
 reprepro_dir="t/${package_name}"
 dl_path=""
 
@@ -19,7 +19,7 @@ debian_diff=""
 
 clear_changelog="enable"
 
-bullseye_version="~bullseye+20250807"
-bookworm_version="~bookworm+20250807"
-trixie_version="~trixie+20250807"
-noble_version="~noble+20250807"
+bookworm_version="~bookworm+20250828"
+trixie_version="~trixie+20250828"
+forky_version="~forky+20250828"
+noble_version="~noble+20250828"
