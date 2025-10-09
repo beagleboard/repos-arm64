4db8339270ae7c748b58298552b550f1155e0fd5
Robert Nelson <robertcnelson@gmail.com>
2025-10-09 14:03:35 -0500

    bb-u-boot-am625-osd625-brk (2024.04.20241129.2)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- bb-u-boot-am625-osd625-brk-debian-12/version.sh
+++ bb-u-boot-am625-osd625-brk-debian-12/version.sh
@@ -5,7 +5,7 @@
 
 package_name="bb-u-boot-am625-osd625-brk"
 debian_pkg_name="${package_name}"
-package_version="2024.04.20241129.1"
+package_version="2024.04.20241129.2"
 package_source=""
 src_dir=""
 
@@ -21,3 +21,6 @@ debian_patch=""
 clear_changelog="enable"
 
 bookworm_version="~bookworm+20251009"
+trixie_version="~trixie+20251009"
+forky_version="~forky+20251009"
+noble_version="~noble+20251009"
