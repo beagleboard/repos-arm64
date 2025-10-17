b7ad8ca9cb6d8a898385358d68b30c6f23db3e27
Robert Nelson <robertcnelson@gmail.com>
2025-10-17 12:23:19 -0500

    iwd (3.10-1bbbio0)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- debian-13-iwd/version.sh
+++ debian-13-iwd/version.sh
@@ -6,7 +6,7 @@ mirror="http://http.debian.net/debian"
 
 package_name="iwd"
 debian_pkg_name="${package_name}"
-package_version="3.9"
+package_version="3.10"
 package_source="${debian_pkg_name}_${package_version}.orig.tar.xz"
 src_dir="${package_name}-${package_version}"
 
@@ -15,9 +15,9 @@ git_sha=""
 reprepro_dir="i/${package_name}"
 dl_path="pool/main/${reprepro_dir}/"
 
-debian_version="${package_version}-2"
+debian_version="${package_version}-1"
 debian_untar="${package_name}_${debian_version}.debian.tar.xz"
 debian_patch=""
-local_patch="bbbio1"
+local_patch="bbbio0"
 
-trixie_version="~trixie+20250814"
+trixie_version="~trixie+20251017"
