7489d60c3df9fb141ce235d6152da318c2ebcaa4
Robert Nelson <robertcnelson@gmail.com>
2025-12-02 08:59:24 -0600

    libcamera (0.5.2-3bbbio0)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- debian-13-libcamera/version.sh
+++ debian-13-libcamera/version.sh
@@ -6,7 +6,7 @@ mirror="http://http.debian.net/debian"
 
 package_name="libcamera"
 debian_pkg_name="${package_name}"
-package_version="0.4.0"
+package_version="0.5.2"
 package_source="${debian_pkg_name}_${package_version}.orig.tar.bz2"
 src_dir="${package_name}-v${package_version}"
 
@@ -15,7 +15,7 @@ git_sha=""
 reprepro_dir="libc/${package_name}"
 dl_path="pool/main/${reprepro_dir}/"
 
-debian_version="${package_version}-7"
+debian_version="${package_version}-3"
 debian_untar="${package_name}_${debian_version}.debian.tar.xz"
 debian_patch=""
 local_patch="bbbio0"
