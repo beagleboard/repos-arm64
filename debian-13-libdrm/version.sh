c0ab142f0779236460c8d370240b61c9f070f990
Robert Nelson <robertcnelson@gmail.com>
2025-12-07 11:19:07 -0600

    libdrm (2.4.129-1bbbio0)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- debian-13-libdrm/version.sh
+++ debian-13-libdrm/version.sh
@@ -4,7 +4,7 @@ mirror="http://http.debian.net/debian"
 
 package_name="libdrm"
 debian_pkg_name="${package_name}"
-package_version="2.4.127"
+package_version="2.4.129"
 package_source="${debian_pkg_name}_${package_version}.orig.tar.xz"
 src_dir="${package_name}-${package_version}"
 
@@ -18,4 +18,4 @@ debian_untar="${package_name}_${debian_version}.debian.tar.xz"
 debian_patch=""
 local_patch="bbbio0"
 
-trixie_version="~trixie+20251111"
+trixie_version="~trixie+20251205"
