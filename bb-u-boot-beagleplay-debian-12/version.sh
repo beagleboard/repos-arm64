c2d80d2d8c9ebbade80109689f9de399ee3b27d2
Robert Nelson <robertcnelson@gmail.com>
2026-01-20 11:39:16 -0600

    bb-u-boot-beagleplay (2026.01.20260105.1)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- bb-u-boot-beagleplay-debian-12/version.sh
+++ bb-u-boot-beagleplay-debian-12/version.sh
@@ -5,7 +5,7 @@
 
 package_name="bb-u-boot-beagleplay"
 debian_pkg_name="${package_name}"
-package_version="2026.01.20260105.0"
+package_version="2026.01.20260105.1"
 package_source=""
 src_dir=""
 
@@ -20,7 +20,7 @@ debian_patch=""
 
 clear_changelog="enable"
 
-bookworm_version="~bookworm+20260109"
-trixie_version="~trixie+20260109"
-forky_version="~forky+20260109"
-noble_version="~noble+20260109"
+bookworm_version="~bookworm+20260120"
+trixie_version="~trixie+20260120"
+forky_version="~forky+20260120"
+noble_version="~noble+20260120"
