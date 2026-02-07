ba585370c6325d5b9f7341337e94af7a52ffa9c5
Robert Nelson <robertcnelson@gmail.com>
2026-02-07 17:11:16 -0600

    oourafft (1.0-3bbbio0)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- debian-13-oourafft/version.sh
+++ debian-13-oourafft/version.sh
@@ -6,7 +6,7 @@ package_name="oourafft"
 debian_pkg_name="${package_name}"
 package_version="1.0"
 package_source="${debian_pkg_name}_${package_version}.orig.tar.gz"
-src_dir="${package_name}-${package_version}"
+src_dir="OouraFFT-${package_version}"
 
 git_repo=""
 git_sha=""
