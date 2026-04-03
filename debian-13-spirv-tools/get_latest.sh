2bece078025510c36fb69c870435a8f96050aad4
Robert Nelson <robertcnelson@gmail.com>
2026-04-03 09:24:35 -0500

    add: debian-13-spirv-tools
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

---
+++ debian-13-spirv-tools/get_latest.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+version="main/s/spirv-tools/spirv-tools_2026.1-1"
+
+if [ -d ./dl/ ] ; then
+	rm -rf ./dl/ || true
+fi
+mkdir ./dl/ || true
+cd ./dl/
+dget -u http://deb.debian.org/debian/pool/${version}.dsc
+cd ../
---
+++ debian-13-spirv-tools/version.sh
@@ -0,0 +1,22 @@
+#!/bin/bash -e
+
+mirror="http://http.debian.net/debian"
+
+package_name="spirv-tools"
+debian_pkg_name="${package_name}"
+package_version="2026.1"
+package_source="${debian_pkg_name}_${package_version}.orig.tar.gz"
+#src_dir="${package_name}-${package_version}"
+src_dir="KhronosGroup-SPIRV-Tools-fbe4f3a"
+
+git_repo=""
+git_sha=""
+reprepro_dir="s/${package_name}"
+dl_path="pool/main/${reprepro_dir}/"
+
+debian_version="${package_version}-1"
+debian_untar="${package_name}_${debian_version}.debian.tar.xz"
+debian_patch=""
+local_patch="bbbio0"
+
+trixie_version="~trixie+20260403"
