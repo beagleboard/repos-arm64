45b6bfc91bf24cec13b806f7e41e5fd54badb885
Robert Nelson <robertcnelson@gmail.com>
2026-04-11 17:48:24 -0500

    backport: llvm-toolchain-21_21.1.4-5
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- debian-bpo13-llvm-toolchain-21/version.sh
+++ debian-bpo13-llvm-toolchain-21/version.sh
@@ -15,10 +15,12 @@
 #https://snapshot.debian.org/archive/debian/20251023T024602Z/pool/main/l/llvm-toolchain-21/llvm-toolchain-21_21.1.4-2.dsc
 #20260411
 #https://snapshot.debian.org/archive/debian/20251024T204237Z/pool/main/l/llvm-toolchain-21/llvm-toolchain-21_21.1.4-3.dsc
+#20260411
+#https://snapshot.debian.org/archive/debian/20251030T144357Z/pool/main/l/llvm-toolchain-21/llvm-toolchain-21_21.1.4-5.dsc
 #https://packages.debian.org/source/forky/llvm-toolchain-21
 
 #mirror="https://deb.debian.org/debian"
-mirror="https://snapshot.debian.org/archive/debian/20251024T204237Z"
+mirror="https://snapshot.debian.org/archive/debian/20251030T144357Z"
 
 package_name="llvm-toolchain-21"
 debian_pkg_name="${package_name}"
@@ -29,7 +31,7 @@ git_sha=""
 reprepro_dir="l/${package_name}"
 dl_path="pool/main/${reprepro_dir}/"
 
-debian_version="${package_version}-3"
+debian_version="${package_version}-5"
 debian_untar="${package_name}_${debian_version}.debian.tar.xz"
 debian_patch=""
 
