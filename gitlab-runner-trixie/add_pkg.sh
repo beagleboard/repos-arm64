7315a6161368fe133d4aa84bb074a916e84c7486
Robert Nelson <robertcnelson@gmail.com>
2026-04-11 11:47:47 -0500

    fixes
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- gitlab-runner-trixie/add_pkg.sh
+++ gitlab-runner-trixie/add_pkg.sh
@@ -6,12 +6,12 @@ base="/var/www/html/repos/"
 outgoing="/var/www/html/farm/outgoing"
 
 run () {
-	apt-get download gitlab-runner:arm64=${build} || true
+	apt-get download gitlab-runner:${deb_arch}=${build} || true
 	apt-get download gitlab-runner-helper-images=${build} || true
 	sync
-	if [ -f ./gitlab-runner_${build}_arm64.deb ] ; then
+	if [ -f ./gitlab-runner_${build}_${deb_arch}.deb ] ; then
 		if [ -f ./gitlab-runner-helper-images_${build}_all.deb ] ; then
-			reprepro -b ${repo} -C main includedeb ${suite} ./gitlab-runner_${build}_arm64.deb
+			reprepro -b ${repo} -C main includedeb ${suite} ./gitlab-runner_${build}_${deb_arch}.deb
 			reprepro -b ${repo} -C main includedeb ${suite} ./gitlab-runner-helper-images_${build}_all.deb
 		fi
 	fi
