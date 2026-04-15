9b2e0e7e907c1ed37e2f81d39a9920e5069aefce
Robert Nelson <robertcnelson@gmail.com>
2026-04-15 17:58:08 -0500

    spirv-llvm-translator-21 (21.1.2-1bbbio0)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- debian-13-spirv-llvm-translator-21/get_latest.sh
+++ debian-13-spirv-llvm-translator-21/get_latest.sh
@@ -4,9 +4,10 @@
 #https://snapshot.debian.org/archive/debian/20250925T142947Z/pool/main/s/spirv-llvm-translator-21/spirv-llvm-translator-21_21.1.0-2.dsc
 #https://snapshot.debian.org/archive/debian/20250929T143752Z/pool/main/s/spirv-llvm-translator-21/spirv-llvm-translator-21_21.1.0-3.dsc
 #https://snapshot.debian.org/archive/debian/20251013T025134Z/pool/main/s/spirv-llvm-translator-21/spirv-llvm-translator-21_21.1.1-1.dsc
+#https://snapshot.debian.org/archive/debian/20251214T024003Z/pool/main/s/spirv-llvm-translator-21/spirv-llvm-translator-21_21.1.2-1.dsc
 #https://packages.debian.org/source/forky/spirv-llvm-translator-21
 
-version="20251013T025134Z/pool/main/s/spirv-llvm-translator-21/spirv-llvm-translator-21_21.1.1-1"
+version="20251214T024003Z/pool/main/s/spirv-llvm-translator-21/spirv-llvm-translator-21_21.1.2-1"
 
 if [ -d ./dl/ ] ; then
 	rm -rf ./dl/ || true
--- debian-13-spirv-llvm-translator-21/version.sh
+++ debian-13-spirv-llvm-translator-21/version.sh
@@ -4,14 +4,15 @@
 #https://snapshot.debian.org/archive/debian/20250925T142947Z/pool/main/s/spirv-llvm-translator-21/spirv-llvm-translator-21_21.1.0-2.dsc
 #https://snapshot.debian.org/archive/debian/20250929T143752Z/pool/main/s/spirv-llvm-translator-21/spirv-llvm-translator-21_21.1.0-3.dsc
 #https://snapshot.debian.org/archive/debian/20251013T025134Z/pool/main/s/spirv-llvm-translator-21/spirv-llvm-translator-21_21.1.1-1.dsc
+#https://snapshot.debian.org/archive/debian/20251214T024003Z/pool/main/s/spirv-llvm-translator-21/spirv-llvm-translator-21_21.1.2-1.dsc
 #https://packages.debian.org/source/forky/spirv-llvm-translator-21
 
 #mirror="https://deb.debian.org/debian"
-mirror="https://snapshot.debian.org/archive/debian/20251013T025134Z"
+mirror="https://snapshot.debian.org/archive/debian/20251214T024003Z"
 
 package_name="spirv-llvm-translator-21"
 debian_pkg_name="${package_name}"
-package_version="21.1.1"
+package_version="21.1.2"
 package_source="${debian_pkg_name}_${package_version}.orig.tar.gz"
 src_dir="SPIRV-LLVM-Translator-${package_version}"
 
@@ -25,4 +26,4 @@ debian_untar="${package_name}_${debian_version}.debian.tar.xz"
 debian_patch=""
 local_patch="bbbio0"
 
-trixie_version="~trixie+20260409"
+trixie_version="~trixie+20260415"
