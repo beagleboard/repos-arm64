88fcbf32e289b6de505f2910f117c5eb87436835
Robert Nelson <robertcnelson@gmail.com>
2026-04-16 21:25:31 -0500

    backport xserver
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

---
+++ debian-bpo13-xorg-server/version.sh
@@ -0,0 +1,24 @@
+#!/bin/bash -e
+
+#https://snapshot.debian.org/archive/debian/20250627T143139Z/pool/main/x/xorg-server/xorg-server_21.1.18-1.dsc
+
+#mirror="https://deb.debian.org/debian"
+mirror="https://snapshot.debian.org/archive/debian/20250627T143139Z"
+
+package_name="xorg-server"
+debian_pkg_name="${package_name}"
+epoch="2"
+package_version="21.1.18"
+
+git_repo=""
+git_sha=""
+reprepro_dir="x/${package_name}"
+dl_path="pool/main/${reprepro_dir}/"
+
+debian_version="${package_version}-1"
+debian_untar="${package_name}_${debian_version}.debian.tar.xz"
+debian_patch=""
+
+dist="debian-trixie-arm64"
+suite="trixie"
+deb_arch="arm64"
