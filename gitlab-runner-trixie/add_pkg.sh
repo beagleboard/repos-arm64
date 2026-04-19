27f138c34d46ad202db6bcebe41bf90c7daa7107
Robert Nelson <robertcnelson@gmail.com>
2026-04-19 10:31:16 -0500

    bump gitlab
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- gitlab-runner-trixie/add_pkg.sh
+++ gitlab-runner-trixie/add_pkg.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-build="18.10.1-1"
-
 base="/var/www/html/repos/"
 outgoing="/var/www/html/farm/outgoing"
 
 run () {
-	apt-get download gitlab-runner:${deb_arch}=${build} || true
-	apt-get download gitlab-runner-helper-images=${build} || true
+	latest=$(apt-cache madison gitlab-runner | grep packages.gitlab.com | awk '{print $3}' | sort -V | tail -1)
+	echo "latest: ${latest}"
+	apt-get download gitlab-runner:${deb_arch}=${latest} || true
+	apt-get download gitlab-runner-helper-images=${latest} || true
 	sync
-	if [ -f ./gitlab-runner_${build}_${deb_arch}.deb ] ; then
-		if [ -f ./gitlab-runner-helper-images_${build}_all.deb ] ; then
-			reprepro -b ${repo} -C main includedeb ${suite} ./gitlab-runner_${build}_${deb_arch}.deb
-			reprepro -b ${repo} -C main includedeb ${suite} ./gitlab-runner-helper-images_${build}_all.deb
+	if [ -f ./gitlab-runner_${latest}_${deb_arch}.deb ] ; then
+		if [ -f ./gitlab-runner-helper-images_${latest}_all.deb ] ; then
+			reprepro -b ${repo} -C main includedeb ${suite} ./gitlab-runner_${latest}_${deb_arch}.deb
+			reprepro -b ${repo} -C main includedeb ${suite} ./gitlab-runner-helper-images_${latest}_all.deb
 		fi
 	fi
 }
