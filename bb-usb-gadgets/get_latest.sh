d6f149a144ecf4c542192a36b4b50ae5aecde9ea
Robert Nelson <robertcnelson@gmail.com>
2026-04-21 16:59:24 -0500

    bb-usb-gadgets (1.20260421.3)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

---
+++ bb-usb-gadgets/get_latest.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+dl_src () {
+	if [ -d ./src/ ] ; then
+		rm -rf ./src/
+	fi
+
+	git clone https://openbeagle.org/beagleboard/usr-customizations.git ./src/
+}
+
+copy_all () {
+#	cp -v ./src${wdir}${wfile} ./suite/bookworm/debian/
+	cp -v ./src${wdir}${wfile} ./suite/trixie/debian/
+	cp -v ./src${wdir}${wfile} ./suite/forky/debian/
+#	cp -v ./src${wdir}${wfile} ./suite/noble/debian/
+}
+
+dl_src
+
+wfile="bb-start-acm-ncm-rndis-old-gadget"     ; wdir="/arm64/usr/bin/" ; copy_all
+wfile="bb-start-usb-gadgets"                  ; wdir="/arm32/usr/bin/" ; copy_all
