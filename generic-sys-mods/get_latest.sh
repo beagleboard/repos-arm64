4be18d1bf2f0f791c5bea713548c12189e41185f
Robert Nelson <robertcnelson@gmail.com>
2026-04-22 13:16:46 -0500

    generic-sys-mods (1.20260422.0)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- generic-sys-mods/get_latest.sh
+++ generic-sys-mods/get_latest.sh
@@ -16,6 +16,15 @@ dl_src () {
 	git clone https://openbeagle.org/beagleboard/${repo}.git ./src/
 }
 
+dl_usr_src () {
+	if [ -d ./src/ ] ; then
+		rm -rf ./src/
+	fi
+
+	#Mirror: https://github.com/beagleboard/customizations
+	git clone ssh://git@forgejo.gfnd.rcn-ee.org:222/BeagleBoard.org/usr-customizations.git ./src/
+}
+
 copy_some () {
 	cp -v ./src/${wfile} ./suite/bookworm/debian/
 	cp -v ./src/${wfile} ./suite/trixie/debian/
@@ -78,3 +87,13 @@ repo="beagle-camera-setup"
 dl_src
 
 wfile="beagle-camera-setup" ; copy_all
+
+dl_usr_src
+
+wfile="arm64/usr/bin/beagle-dtb-source" ; copy_all
+
+wfile="arm64/usr/sbin/bb-growpart" ; copy_all
+wfile="arm64/usr/sbin/bb-regenerate-ssh-host-keys" ; copy_all
+wfile="arm64/usr/sbin/bb-resizefs" ; copy_all
+wfile="arm64/usr/sbin/bb-setup-mac-address" ; copy_all
+#
--- generic-sys-mods/version.sh
+++ generic-sys-mods/version.sh
@@ -2,7 +2,7 @@
 
 package_name="generic-sys-mods"
 debian_pkg_name="${package_name}"
-package_version="1.20250905.0"
+package_version="1.20260422.0"
 package_source=""
 src_dir=""
 
@@ -17,7 +17,7 @@ debian_patch=""
 
 clear_changelog="enable"
 
-bookworm_version="~bookworm+20250905"
-trixie_version="~trixie+20250905"
-forky_version="~forky+20250905"
-noble_version="~noble+20250905"
+bookworm_version="~bookworm+20260422"
+trixie_version="~trixie+20260422"
+forky_version="~forky+20260422"
+noble_version="~noble+20260422"
