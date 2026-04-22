db572ecccbf2cbe57d5103e8ab1a9090b6dd300a
Robert Nelson <robertcnelson@gmail.com>
2026-04-22 14:19:11 -0500

    add: debian-13-shaderc
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

---
+++ debian-13-shaderc/version.sh
@@ -0,0 +1,21 @@
+#!/bin/bash -e
+
+mirror="http://http.debian.net/debian"
+
+package_name="shaderc"
+debian_pkg_name="${package_name}"
+package_version="2026.1"
+package_source="${debian_pkg_name}_${package_version}.orig.tar.gz"
+src_dir="google-shaderc-75f8c09"
+
+git_repo=""
+git_sha=""
+reprepro_dir="s/${package_name}"
+dl_path="pool/main/${reprepro_dir}/"
+
+debian_version="${package_version}-1"
+debian_untar="${package_name}_${debian_version}.debian.tar.xz"
+debian_patch=""
+local_patch="bbbio0"
+
+trixie_version="~trixie+20260421"
