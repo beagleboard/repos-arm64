2969cb7207d827a333c658d6a27310b064a7ee03
Robert Nelson <robertcnelson@gmail.com>
2026-04-27 12:13:48 -0500

    powervr-firmware (0.0.0-git20260402.0)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- powervr-firmware/version.sh
+++ powervr-firmware/version.sh
@@ -4,7 +4,7 @@
 
 package_name="powervr-firmware"
 debian_pkg_name="${package_name}"
-package_version="0.0.0-git20250922.0"
+package_version="0.0.0-git20260402.0"
 package_source=""
 src_dir=""
 
@@ -19,7 +19,7 @@ debian_diff=""
 
 clear_changelog="enable"
 
-bookworm_version="~bookworm+20260110"
-trixie_version="~trixie+20260110"
-forky_version="~forky+20260110"
-noble_version="~noble+20260110"
+bookworm_version="~bookworm+20260427"
+trixie_version="~trixie+20260427"
+forky_version="~forky+20260427"
+noble_version="~noble+20260427"
