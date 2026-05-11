8b825f2dd3631091c9a59c64a5233c4e02f56e31
Robert Nelson <robertcnelson@gmail.com>
2026-05-11 16:15:11 -0500

    backport: ccache (4.13.6-1)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

---
+++ debian-bpo13-ccache/version.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -e
+
+mirror="https://deb.debian.org/debian"
+
+package_name="ccache"
+debian_pkg_name="${package_name}"
+package_version="4.13.6"
+
+reprepro_dir="c/${package_name}"
+dl_path="pool/main/${reprepro_dir}/"
+
+debian_version="${package_version}-1"
+
+dist="debian-trixie-amd64"
+suite="trixie"
+deb_arch="amd64"
