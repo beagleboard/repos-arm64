6b1ece063c744df18d66fd2636675e4e3a6a4bc7
Robert Nelson <robertcnelson@gmail.com>
2026-06-01 10:29:58 -0500

    backport: debootstrap 1.0.144
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- debian-bpo13-debootstrap/version.sh
+++ debian-bpo13-debootstrap/version.sh
@@ -4,16 +4,13 @@ mirror="https://deb.debian.org/debian"
 
 package_name="debootstrap"
 debian_pkg_name="${package_name}"
-package_version="1.0.143"
+package_version="1.0.144"
 
-git_repo=""
-git_sha=""
 reprepro_dir="d/${package_name}"
 dl_path="pool/main/${reprepro_dir}/"
 
 debian_version="${package_version}"
 debian_untar="${package_name}_${debian_version}.debian.tar.xz"
-debian_patch=""
 
 dist="debian-trixie-arm64"
 suite="trixie"
