8a988f5261c78db05169549d56f5e10e5e857a15
Robert Nelson <robertcnelson@gmail.com>
2026-07-24 09:19:03 -0500

    add puller
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

---
+++ tools/pull.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# SPDX-FileCopyrightText: Robert Nelson <robertcnelson@gmail.com>
+#
+# SPDX-License-Identifier: MIT
+
+git pull --no-edit https://github.com/beagleboard/repos-arm64.git main
