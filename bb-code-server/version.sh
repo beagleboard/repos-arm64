65de159e8717325eb9bf0d31a624a9729b2ff75e
Robert Nelson <robertcnelson@gmail.com>
2026-08-04 09:20:00 -0500

    bb-code-server (4.131.0-0bbbio0)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- bb-code-server/version.sh
+++ bb-code-server/version.sh
@@ -3,7 +3,7 @@
 #https://github.com/coder/code-server/releases
 
 arch="arm64"
-ver="4.130.0"
+ver="4.131.0"
 
 package_name="bb-code-server"
 debian_pkg_name="${package_name}"
@@ -24,7 +24,7 @@ debian_dl_1="https://github.com/coder/code-server/releases/download/v${ver}/code
 
 clear_changelog="enable"
 
-bookworm_version="~bookworm+20260728"
-trixie_version="~trixie+20260728"
-forky_version="~forky+20260728"
-noble_version="~noble+20260728"
+bookworm_version="~bookworm+20260804"
+trixie_version="~trixie+20260804"
+forky_version="~forky+20260804"
+noble_version="~noble+20260804"
