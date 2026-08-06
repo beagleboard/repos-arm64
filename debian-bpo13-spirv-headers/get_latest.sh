547ba75ad5ceec44c3802f8c45de188d08e7508e
Robert Nelson <robertcnelson@gmail.com>
2026-08-06 10:16:19 -0500

    backport: spirv-headers_1.6.1+1.4.357.0-1
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- debian-bpo13-spirv-headers/get_latest.sh
+++ debian-bpo13-spirv-headers/get_latest.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-version="main/s/spirv-headers/spirv-headers_1.6.1+1.4.341.0-1"
+version="main/s/spirv-headers/spirv-headers_1.6.1+1.4.357.0-1"
 
 if [ -d ./dl/ ] ; then
 	rm -rf ./dl/ || true
