64acaa97e2bac0a872c63116ebfda3bd4db58cea
Robert Nelson <robertcnelson@gmail.com>
2026-08-18 14:37:35 -0500

    Revert "backport: ggml (0.19.0-1)"
    This reverts commit 4197006c49a17ad72eb0845f28883f663a2a395b.
    
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- debian-bpo13-ggml/version.sh
+++ debian-bpo13-ggml/version.sh
@@ -4,12 +4,12 @@ mirror="https://deb.debian.org/debian"
 
 package_name="ggml"
 debian_pkg_name="${package_name}"
-package_version="0.19.0"
+package_version="0.18.1"
 
 reprepro_dir="g/${package_name}"
 dl_path="pool/main/${reprepro_dir}/"
 
-debian_version="${package_version}-1"
+debian_version="${package_version}-1~bpo13+1"
 
 dist="debian-trixie-arm64"
 suite="trixie"
