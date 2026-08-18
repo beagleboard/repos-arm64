f87f17c9065ffd22eca0c957baf84bdc90754303
Robert Nelson <robertcnelson@gmail.com>
2026-08-18 11:42:36 -0500

    backport: pkg-rocm-tools (0.9.8)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- debian-bpo13-pkg-rocm-tools/version.sh
+++ debian-bpo13-pkg-rocm-tools/version.sh
@@ -4,12 +4,12 @@ mirror="https://deb.debian.org/debian"
 
 package_name="pkg-rocm-tools"
 debian_pkg_name="${package_name}"
-package_version="0.9.3"
+package_version="0.9.8"
 
 reprepro_dir="p/${package_name}"
 dl_path="pool/main/${reprepro_dir}/"
 
-debian_version="${package_version}~bpo13+1"
+debian_version="${package_version}"
 
 dist="debian-trixie-arm64"
 suite="trixie"
