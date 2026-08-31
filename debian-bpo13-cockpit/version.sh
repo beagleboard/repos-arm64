43e2703749c01f4efcee90f24a97544e11c2f207
Robert Nelson <robertcnelson@gmail.com>
2026-08-31 09:27:50 -0500

    backport: cockpit (366-1~bpo13+1)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- debian-bpo13-cockpit/version.sh
+++ debian-bpo13-cockpit/version.sh
@@ -4,7 +4,7 @@ mirror="https://deb.debian.org/debian"
 
 package_name="cockpit"
 debian_pkg_name="${package_name}"
-package_version="365"
+package_version="366"
 
 reprepro_dir="c/${package_name}"
 dl_path="pool/main/${reprepro_dir}/"
