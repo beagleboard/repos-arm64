ddc2ca99f47e0b8f9a91744c46da2942b8b8391f
Robert Nelson <robertcnelson@gmail.com>
2026-09-01 09:07:44 -0500

    generic-sys-mods (1.20260901.0)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- generic-sys-mods/version.sh
+++ generic-sys-mods/version.sh
@@ -2,7 +2,7 @@
 
 package_name="generic-sys-mods"
 debian_pkg_name="${package_name}"
-package_version="1.20260720.0"
+package_version="1.20260901.0"
 package_source=""
 src_dir=""
 
@@ -17,7 +17,7 @@ debian_patch=""
 
 clear_changelog="enable"
 
-bookworm_version="~bookworm+20260720"
-trixie_version="~trixie+20260720"
-forky_version="~forky+20260720"
-noble_version="~noble+20260720"
+bookworm_version="~bookworm+20260901"
+trixie_version="~trixie+20260901"
+forky_version="~forky+20260901"
+noble_version="~noble+20260901"
