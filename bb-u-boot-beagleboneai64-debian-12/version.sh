177a769eef0bf75573a169b7eab0c65add1de8f0
Robert Nelson <robertcnelson@gmail.com>
2026-09-10 19:16:44 -0500

    bb-u-boot-beagleboneai64 (2026.07.20260706.0)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- bb-u-boot-beagleboneai64-debian-12/version.sh
+++ bb-u-boot-beagleboneai64-debian-12/version.sh
@@ -5,7 +5,7 @@
 
 package_name="bb-u-boot-beagleboneai64"
 debian_pkg_name="${package_name}"
-package_version="2026.01.20260105.2"
+package_version="2026.07.20260706.0"
 package_source=""
 src_dir=""
 
@@ -20,7 +20,7 @@ debian_patch=""
 
 clear_changelog="enable"
 
-bookworm_version="~bookworm+20260210"
-trixie_version="~trixie+20260210"
-forky_version="~forky+20260210"
-noble_version="~noble+20260210"
+bookworm_version="~bookworm+20260910"
+trixie_version="~trixie+20260910"
+forky_version="~forky+20260910"
+noble_version="~noble+20260910"
