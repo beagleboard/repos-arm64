269d726bc83e324e4a250dcc58d423df82f5ed22
Robert Nelson <robertcnelson@gmail.com>
2026-09-16 14:18:29 -0500

    backport: python-spdx-tools (0.8.5-1)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- debian-bpo13-python-spdx-tools/version.sh
+++ debian-bpo13-python-spdx-tools/version.sh
@@ -2,14 +2,14 @@
 
 mirror="https://deb.debian.org/debian"
 
-package_name="debsbom"
+package_name="python-spdx-tools"
 debian_pkg_name="${package_name}"
-package_version="0.10.1"
+package_version="0.8.5"
 
-reprepro_dir="d/${package_name}"
+reprepro_dir="p/${package_name}"
 dl_path="pool/main/${reprepro_dir}/"
 
-debian_version="${package_version}-1~bpo13+1"
+debian_version="${package_version}-1"
 
 dist="debian-trixie-arm64"
 suite="trixie"
